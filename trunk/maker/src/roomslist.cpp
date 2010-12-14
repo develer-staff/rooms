b326e783373b6f3200215c067db8621130c245a6
Daniel Sanchez <codeverse@develer.com>
2010-12-14 11:18:09 +0100

    implementing context menu in rooms_list

--- trunk/maker/src/roomslist.cpp
+++ trunk/maker/src/roomslist.cpp
@@ -1,6 +1,26 @@
 #include "roomslist.h"
+#include <QMenu>
 
 RoomsList::RoomsList(QWidget *parent) :
     QListView(parent)
 {
+    connect(this, SIGNAL(customContextMenuRequested(const QPoint &)),
+            this, SLOT(showContextMenu(const QPoint &)));
+}
+
+void RoomsList::setWorld(World *world)
+{
+    this->world = world;
+}
+
+void RoomsList::showContextMenu(const QPoint &point)
+{
+    QMenu *menu = new QMenu;
+    menu->addAction(tr("Add a room"), this, SLOT(addRoom()));
+    menu->exec(this->mapToGlobal(point));
+}
+
+void RoomsList::addRoom()
+{
+    //world->roomsModel()
 }
