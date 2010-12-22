develer-staff/rooms

commit 9befb17e5c0e6a93ab1d3a8e2bedf3335a3bbdc3

Refactoring: cleaning code and global namespace.

trunk/engine/src/engine.cpp
@@ -86,7 +86,7 @@ bool Engine::loadWorld(const string filename)
         log("Loading world from " + filename, 2);
         TiXmlDocument document(filename.c_str());
         int width = 0, height = 0;
-        if (!xmlCheckDoc(&document)) throw "ERROR: wrong xml document!";
+        if (!std::xmlCheckDoc(&document)) throw "ERROR: wrong xml document!";
         TiXmlElement *root = document.RootElement();
         //Load World attributes
         log(root->Attribute("name"), 2);
@@ -97,15 +97,15 @@ bool Engine::loadWorld(const string filename)
         //TODO: manage different screen resolutions
         //Loading from xml
         std::vector <TiXmlElement *> images =
-            xmlGetAllChilds(root->FirstChildElement("images"), "img");
+            std::xmlGetAllChilds(root->FirstChildElement("images"), "img");
         std::vector <TiXmlElement *> rooms =
-            xmlGetAllChilds(root->FirstChildElement("rooms"), "room");
+            std::xmlGetAllChilds(root->FirstChildElement("rooms"), "room");
         std::vector <TiXmlElement *> events =
-            xmlGetAllChilds(root->FirstChildElement("events"), "event");
+            std::xmlGetAllChilds(root->FirstChildElement("events"), "event");
         std::vector <TiXmlElement *> items =
-            xmlGetAllChilds(root->FirstChildElement("items"), "item");
+            std::xmlGetAllChilds(root->FirstChildElement("items"), "item");
         std::vector <TiXmlElement *> vars =
-            xmlGetAllChilds(root->FirstChildElement("vars"), "var");
+            std::xmlGetAllChilds(root->FirstChildElement("vars"), "var");
         //Populating model
         createImgsFromXml(images);
         createEventsFromXml(events);
@@ -162,13 +162,13 @@ void Engine::createEventsFromXml(std::vector <TiXmlElement *> events)
         }
         //create items parameters
         std::vector <TiXmlElement *> ireqs =
-            xmlGetAllChilds((*i)->FirstChildElement("requirements"), "item_req");
+            std::xmlGetAllChilds((*i)->FirstChildElement("requirements"), "item_req");
         for (std::vector<TiXmlElement *>::iterator j = ireqs.begin();
              j != ireqs.end(); ++j)
             event->addItemReq((*j)->Attribute("id"), (*j)->Attribute("value"));
         //create var parameters
         std::vector <TiXmlElement *> vreqs =
-            xmlGetAllChilds((*i)->FirstChildElement("requirements"), "var_req");
+            std::xmlGetAllChilds((*i)->FirstChildElement("requirements"), "var_req");
         for (std::vector<TiXmlElement *>::iterator j = vreqs.begin();
              j != vreqs.end(); ++j)
         {
@@ -178,7 +178,7 @@ void Engine::createEventsFromXml(std::vector <TiXmlElement *> events)
         }
         //create actions
         std::vector <TiXmlElement *> actions =
-            xmlGetAllChilds((*i)->FirstChildElement("actions_if"), "action");
+            std::xmlGetAllChilds((*i)->FirstChildElement("actions_if"), "action");
         for (std::vector<TiXmlElement *>::iterator j = actions.begin();
              j != actions.end(); ++j)
         {
@@ -189,7 +189,7 @@ void Engine::createEventsFromXml(std::vector <TiXmlElement *> events)
                 continue;
             }
             std::vector <TiXmlElement *> params =
-                xmlGetAllChilds(*j, "param");
+                std::xmlGetAllChilds(*j, "param");
             for (std::vector<TiXmlElement *>::iterator z = params.begin();
                  z != params.end(); ++z)
                 act->pushParam((*z)->Attribute("value"));
@@ -208,7 +208,7 @@ void Engine::createRoomsFromXml(std::vector <TiXmlElement *> rooms)
             continue;
         }
         std::vector <TiXmlElement *> areas =
-            xmlGetAllChilds((*i)->FirstChildElement("areas"), "area");
+            std::xmlGetAllChilds((*i)->FirstChildElement("areas"), "area");
         for (std::vector<TiXmlElement *>::iterator j = areas.begin();
              j != areas.end(); ++j)
         {

trunk/engine/src/roomsmanager.cpp
@@ -4,7 +4,7 @@
 #include "item.h"
 #include "area.h"
 
-template <class T> T *freeElements(std::map<string , T *> &container)
+template <class T> void std::freeElements(std::map<string , T *> &container)
 {
     for (typename std::map<string, T *>::iterator i = container.begin();
          i != container.end(); ++i)
@@ -15,7 +15,7 @@ template <class T> T *freeElements(std::map<string , T *> &container)
     container.clear();
 }
 
-template <class T> T *getElement(string name, std::map<string, T *> container)
+template <class T> T *std::getElement(string name, std::map<string, T *> container)
 {
     typename std::map<string, T *>::iterator i = container.find(name);
     if (i == container.end())
@@ -32,9 +32,9 @@ RoomsManager::RoomsManager(Engine *eng)
 
 RoomsManager::~RoomsManager()
 {
-    freeElements(rooms);
-    freeElements(items);
-    freeElements(areas);
+    std::freeElements(rooms);
+    std::freeElements(items);
+    std::freeElements(areas);
 }
 
 void RoomsManager::name(const string name)
@@ -131,17 +131,17 @@ void RoomsManager::moveItem(const string name, const string dest)
 
 Room * RoomsManager::room(const string name)
 {
-    return getElement(name, rooms);
+    return std::getElement(name, rooms);
 }
 
 Area * RoomsManager::area(const string name)
 {
-    return getElement(name, areas);
+    return std::getElement(name, areas);
 }
 
 Item * RoomsManager::item(const string name)
 {
-    return getElement(name, items);
+    return std::getElement(name, items);
 }
 
 void RoomsManager::currentRoom(const string name)

trunk/engine/src/roomsmanager.h
@@ -12,13 +12,16 @@ class Item;
 
 using std::string;
 
+
+namespace std
+{
+    template <class T> T *getElement(string name, std::map<string, T *> container);
+    template <class T> void freeElements(std::map<string , T *> &container);
+}
+
 /*! \brief Rooms interface.
  *         Safely creates and garbage-collects rooms, areas and items.
  */
-
-template <class T> T *getElement(string name, std::map<string, T *> container);
-template <class T> T *freeElements(std::map<string , T *> &container);
-
 class RoomsManager
 {
     private:

trunk/engine/src/xmlutils.cpp
@@ -1,19 +1,19 @@
 #include "xmlutils.h"
 
-bool xmlCheckDoc(TiXmlDocument *doc)
+bool std::xmlCheckDoc(TiXmlDocument *doc)
 {
     if (doc)
         return doc->LoadFile() &&
-               xmlCheckRoot(doc->RootElement()) &&
-               xmlCheckImages(doc->RootElement()->FirstChildElement("images")) &&
-               xmlCheckVars(doc->RootElement()->FirstChildElement("vars")) &&
-               xmlCheckEvents(doc->RootElement()->FirstChildElement("events")) &&
-               xmlCheckRooms(doc->RootElement()->FirstChildElement("rooms")) &&
-               xmlCheckRooms(doc->RootElement()->FirstChildElement("items"));
+               std::xmlCheckRoot(doc->RootElement()) &&
+               std::xmlCheckImages(doc->RootElement()->FirstChildElement("images")) &&
+               std::xmlCheckVars(doc->RootElement()->FirstChildElement("vars")) &&
+               std::xmlCheckEvents(doc->RootElement()->FirstChildElement("events")) &&
+               std::xmlCheckRooms(doc->RootElement()->FirstChildElement("rooms")) &&
+               std::xmlCheckRooms(doc->RootElement()->FirstChildElement("items"));
     return false;
 }
 
-bool xmlCheckRoot(TiXmlElement *elem)
+bool std::xmlCheckRoot(TiXmlElement *elem)
 {
     int tmp;
     if (!elem)
@@ -29,7 +29,7 @@ bool xmlCheckRoot(TiXmlElement *elem)
     return true;
 }
 
-bool xmlCheckImages(TiXmlElement *elem)
+bool std::xmlCheckImages(TiXmlElement *elem)
 {
     if (elem)
         for (TiXmlElement *i = elem->FirstChildElement("img"); i != 0;
@@ -42,7 +42,7 @@ bool xmlCheckImages(TiXmlElement *elem)
     return true;
 }
 
-bool xmlCheckVars(TiXmlElement *elem)
+bool std::xmlCheckVars(TiXmlElement *elem)
 {
     if (elem)
         for (TiXmlElement *i = elem->FirstChildElement("var"); i != 0;
@@ -57,7 +57,7 @@ bool xmlCheckVars(TiXmlElement *elem)
 }
 
 
-bool xmlCheckEvents(TiXmlElement *elem)
+bool std::xmlCheckEvents(TiXmlElement *elem)
 {
     if (elem)
     {
@@ -73,7 +73,7 @@ bool xmlCheckEvents(TiXmlElement *elem)
     return true;
 }
 
-bool xmlCheckActions(TiXmlElement *elem)
+bool std::xmlCheckActions(TiXmlElement *elem)
 {
     if (elem)
         for (TiXmlElement *i = elem->FirstChildElement("action"); i != 0;
@@ -85,7 +85,7 @@ bool xmlCheckActions(TiXmlElement *elem)
     return true;
 }
 
-bool xmlCheckParams(TiXmlElement *elem)
+bool std::xmlCheckParams(TiXmlElement *elem)
 {
     if (elem)
         for (TiXmlElement *i = elem->FirstChildElement("param"); i != 0;
@@ -97,7 +97,7 @@ bool xmlCheckParams(TiXmlElement *elem)
     return true;
 }
 
-bool xmlCheckReqs(TiXmlElement *elem)
+bool std::xmlCheckReqs(TiXmlElement *elem)
 {
     if (elem)
     {
@@ -120,7 +120,7 @@ bool xmlCheckReqs(TiXmlElement *elem)
     return true;
 }
 
-bool xmlCheckRooms(TiXmlElement *elem)
+bool std::xmlCheckRooms(TiXmlElement *elem)
 {
     if (elem)
         for (TiXmlElement *i = elem->FirstChildElement("room"); i != 0;
@@ -134,7 +134,7 @@ bool xmlCheckRooms(TiXmlElement *elem)
     return true;
 }
 
-bool xmlCheckAreas(TiXmlElement *elem)
+bool std::xmlCheckAreas(TiXmlElement *elem)
 {
     int tmp;
     if (elem)
@@ -153,7 +153,7 @@ bool xmlCheckAreas(TiXmlElement *elem)
     return true;
 }
 
-bool xmlCheckItems(TiXmlElement *elem)
+bool std::xmlCheckItems(TiXmlElement *elem)
 {
     int tmp;
     if (elem)
@@ -173,7 +173,7 @@ bool xmlCheckItems(TiXmlElement *elem)
     return true;
 }
 
-bool xmlCheckDoEvents(TiXmlElement *elem)
+bool std::xmlCheckDoEvents(TiXmlElement *elem)
 {
     if (elem)
         for (TiXmlElement *i = elem->FirstChildElement("do_event"); i != 0;
@@ -185,7 +185,7 @@ bool xmlCheckDoEvents(TiXmlElement *elem)
     return true;
 }
 
-std::vector <TiXmlElement *> xmlGetAllChilds(TiXmlElement *elem, std::string id)
+std::vector <TiXmlElement *> std::xmlGetAllChilds(TiXmlElement *elem, std::string id)
 {
     std::vector <TiXmlElement *> childs;
     if (elem)

trunk/engine/src/xmlutils.h
@@ -6,18 +6,21 @@
 #include <string> //std::string
 #include <vector> //std::vector
 
-std::vector <TiXmlElement *> xmlGetAllChilds(TiXmlElement *elem, std::string id);
-bool xmlCheckDoc(TiXmlDocument *doc);
-bool xmlCheckRoot(TiXmlElement *elem);
-bool xmlCheckRooms(TiXmlElement *elem);
-bool xmlCheckImages(TiXmlElement *elem);
-bool xmlCheckVars(TiXmlElement *elem);
-bool xmlCheckEvents(TiXmlElement *elem);
-bool xmlCheckActions(TiXmlElement *elem);
-bool xmlCheckParams(TiXmlElement *elem);
-bool xmlCheckReqs(TiXmlElement *elem);
-bool xmlCheckAreas(TiXmlElement *elem);
-bool xmlCheckItems(TiXmlElement *elem);
-bool xmlCheckDoEvents(TiXmlElement *elem);
+namespace std
+{
+    std::vector <TiXmlElement *> xmlGetAllChilds(TiXmlElement *elem, std::string id);
+    bool xmlCheckDoc(TiXmlDocument *doc);
+    bool xmlCheckRoot(TiXmlElement *elem);
+    bool xmlCheckRooms(TiXmlElement *elem);
+    bool xmlCheckImages(TiXmlElement *elem);
+    bool xmlCheckVars(TiXmlElement *elem);
+    bool xmlCheckEvents(TiXmlElement *elem);
+    bool xmlCheckActions(TiXmlElement *elem);
+    bool xmlCheckParams(TiXmlElement *elem);
+    bool xmlCheckReqs(TiXmlElement *elem);
+    bool xmlCheckAreas(TiXmlElement *elem);
+    bool xmlCheckItems(TiXmlElement *elem);
+    bool xmlCheckDoEvents(TiXmlElement *elem);
+}
 
 #endif // XMLMANAGER_H

trunk/engine/frontend/drawdevice.cpp
@@ -1,5 +1,6 @@
 #include "drawdevice.h"
 
+
 DrawDevice::DrawDevice(Engine *eng, QWidget *parent): QWidget(parent)
 {
     engine = eng;
@@ -35,12 +36,6 @@ bool DrawDevice::loadImage(string id, string filename)
     {
         return false;
     }
-}
-
-bool DrawDevice::fileExists(string filename)
-{
-    std::ifstream ifile(filename.c_str());
-    return ifile;
 }
 
 void DrawDevice::quit(int status)

trunk/engine/frontend/drawdevice.h
@@ -10,6 +10,14 @@
 
 using std::string;
 
+namespace
+{
+    bool fileExists(string filename)
+    {
+        std::ifstream ifile(filename.c_str());
+        return ifile;
+    }
+}
 
 /*! \brief Graphic interface class.
  *         It handles user event and it controls engine.
@@ -33,9 +41,6 @@ class DrawDevice: public QWidget
         void paintEvent(QPaintEvent *event);
         void mousePressEvent(QMouseEvent * event);
         void mouseMoveEvent(QMouseEvent *event);
-    private:
-        bool fileExists(string filename);
-
 };
 
 #endif // DRAWDEVICE_H

commit c0d5f62ed1d62b151b42f6fcf9057c234bd2d9e7

Fixing namespace names error introduced in 9befb17e5c0e6a93ab1d3a8e2bedf3335a3bbdc3

trunk/engine/src/engine.cpp
@@ -86,25 +86,25 @@ bool Engine::loadWorld(const string filename)
     {
         logger.write("Loading world from " + filename, Log::NOTE);
         TiXmlDocument document(filename.c_str());
-        if (!std::xmlCheckDoc(&document)) throw "ERROR: wrong xml document!";
+        if (!xml::xmlCheckDoc(&document)) throw "ERROR: wrong xml document!";
         TiXmlElement *root = document.RootElement();
         //Load World attributes
         logger.write(root->Attribute("name"), Log::NOTE);
-        rooms_mgr->size(std::xmlReadInt(root, "width"),
-                        std::xmlReadInt(root, "height"));
+        rooms_mgr->size(xml::xmlReadInt(root, "width"),
+                        xml::xmlReadInt(root, "height"));
         rooms_mgr->name(root->Attribute("name"));
         //TODO: manage different screen resolutions
         //Loading from xml
         XmlVect images =
-            std::xmlGetAllChilds(root->FirstChildElement("images"), "img");
+            xml::xmlGetAllChilds(root->FirstChildElement("images"), "img");
         XmlVect rooms =
-            std::xmlGetAllChilds(root->FirstChildElement("rooms"), "room");
+            xml::xmlGetAllChilds(root->FirstChildElement("rooms"), "room");
         XmlVect events =
-            std::xmlGetAllChilds(root->FirstChildElement("events"), "event");
+            xml::xmlGetAllChilds(root->FirstChildElement("events"), "event");
         XmlVect items =
-            std::xmlGetAllChilds(root->FirstChildElement("items"), "item");
+            xml::xmlGetAllChilds(root->FirstChildElement("items"), "item");
         XmlVect vars =
-            std::xmlGetAllChilds(root->FirstChildElement("vars"), "var");
+            xml::xmlGetAllChilds(root->FirstChildElement("vars"), "var");
         //Populating model
         createImgsFromXml(images);
         createEventsFromXml(events);
@@ -141,7 +141,7 @@ void Engine::createVarsFromXml(XmlVect vars)
 {
     for (XmlVect::iterator i = vars.begin();
          i != vars.end(); ++i)
-        events_mgr->setVar((*i)->Attribute("id"), std::xmlReadInt((*i), "value"));
+        events_mgr->setVar((*i)->Attribute("id"), xml::xmlReadInt((*i), "value"));
 }
 
 void Engine::createEventsFromXml(XmlVect events)
@@ -150,19 +150,19 @@ void Engine::createEventsFromXml(XmlVect events)
     {
         Event *event = events_mgr->addEvent((*i)->Attribute("id"));
         //create items parameters
-        XmlVect ireqs = std::xmlGetAllChilds((*i)->FirstChildElement("requirements"), "item_req");
+        XmlVect ireqs = xml::xmlGetAllChilds((*i)->FirstChildElement("requirements"), "item_req");
         for (XmlVect::iterator j = ireqs.begin(); j != ireqs.end(); ++j)
             event->addItemReq((*j)->Attribute("id"), (*j)->Attribute("value"));
         //create var parameters
-        XmlVect vreqs = std::xmlGetAllChilds((*i)->FirstChildElement("requirements"), "var_req");
+        XmlVect vreqs = xml::xmlGetAllChilds((*i)->FirstChildElement("requirements"), "var_req");
         for (XmlVect::iterator j = vreqs.begin(); j != vreqs.end(); ++j)
-            event->addVarReq((*j)->Attribute("id"), std::xmlReadInt((*j), "value"));
+            event->addVarReq((*j)->Attribute("id"), xml::xmlReadInt((*j), "value"));
         //create actions
-        XmlVect actions = std::xmlGetAllChilds((*i)->FirstChildElement("actions_if"), "action");
+        XmlVect actions = xml::xmlGetAllChilds((*i)->FirstChildElement("actions_if"), "action");
         for (XmlVect::iterator j = actions.begin(); j != actions.end(); ++j)
         {
             Action *act = event->addAction((*j)->Attribute("id"));
-            XmlVect params = std::xmlGetAllChilds(*j, "param");
+            XmlVect params = xml::xmlGetAllChilds(*j, "param");
             for (XmlVect::iterator z = params.begin(); z != params.end(); ++z)
                 act->pushParam((*z)->Attribute("value"));
         }
@@ -174,15 +174,15 @@ void Engine::createRoomsFromXml(XmlVect rooms)
     for (XmlVect::iterator i = rooms.begin(); i != rooms.end(); ++i)
     {
         rooms_mgr->addRoom((*i)->Attribute("id"), (*i)->Attribute("bg"));
-        XmlVect areas = std::xmlGetAllChilds((*i)->FirstChildElement("areas"), "area");
+        XmlVect areas = xml::xmlGetAllChilds((*i)->FirstChildElement("areas"), "area");
         for (XmlVect::iterator j = areas.begin(); j != areas.end(); ++j)
         {
             rooms_mgr->addArea((*j)->Attribute("id"),
                                (*i)->Attribute("id"),
-                               std::xmlReadInt((*j), "x"),
-                               std::xmlReadInt((*j), "y"),
-                               std::xmlReadInt((*j), "width"),
-                               std::xmlReadInt((*j), "height"),
+                               xml::xmlReadInt((*j), "x"),
+                               xml::xmlReadInt((*j), "y"),
+                               xml::xmlReadInt((*j), "width"),
+                               xml::xmlReadInt((*j), "height"),
                                (*j)->FirstChildElement("do_event")->Attribute("value"));
         }
     }
@@ -194,10 +194,10 @@ void Engine::createItemsFromXml(XmlVect items)
     {
         rooms_mgr->addItem((*i)->Attribute("id"),
                             (*i)->Attribute("room"),
-                            std::xmlReadInt((*i), "x"),
-                            std::xmlReadInt((*i), "y"),
-                            std::xmlReadInt((*i), "width"),
-                            std::xmlReadInt((*i), "height"),
+                            xml::xmlReadInt((*i), "x"),
+                            xml::xmlReadInt((*i), "y"),
+                            xml::xmlReadInt((*i), "width"),
+                            xml::xmlReadInt((*i), "height"),
                             (*i)->FirstChildElement("do_event")->Attribute("value"),
                             (*i)->Attribute("image"));
     }

trunk/engine/src/roomsmanager.cpp
@@ -4,7 +4,7 @@
 #include "item.h"
 #include "area.h"
 
-template <class T> void std::freeElements(std::map<string , T *> &container)
+template <class T> void utils::freeElements(std::map<string , T *> &container)
 {
     for (typename std::map<string, T *>::iterator i = container.begin();
          i != container.end(); ++i)
@@ -15,7 +15,7 @@ template <class T> void std::freeElements(std::map<string , T *> &container)
     container.clear();
 }
 
-template <class T> T *std::getElement(string name, std::map<string, T *> container)
+template <class T> T *utils::getElement(string name, std::map<string, T *> container)
 {
     typename std::map<string, T *>::iterator i = container.find(name);
     if (i == container.end())
@@ -31,8 +31,8 @@ RoomsManager::RoomsManager()
 
 RoomsManager::~RoomsManager()
 {
-    std::freeElements(rooms);
-    std::freeElements(items);
+    utils::freeElements(rooms);
+    utils::freeElements(items);
 }
 
 void RoomsManager::name(const string name)
@@ -112,7 +112,7 @@ void RoomsManager::moveItem(const string name, const string dest)
 
 Room * RoomsManager::room(const string name)
 {
-    return std::getElement(name, rooms);
+    return utils::getElement(name, rooms);
 }
 
 Area * RoomsManager::area(const string name)
@@ -122,7 +122,7 @@ Area * RoomsManager::area(const string name)
 
 Item * RoomsManager::item(const string name)
 {
-    return std::getElement(name, items);
+    return utils::getElement(name, items);
 }
 
 void RoomsManager::setCurrentRoom(const string name)

trunk/engine/src/roomsmanager.h
@@ -12,10 +12,10 @@ class Item;
 using std::string;
 
 
-namespace std
+namespace utils
 {
-    template <class T> T *getElement(string name, std::map<string, T *> container);
     template <class T> void freeElements(std::map<string , T *> &container);
+    template <class T> T *getElement(string name, std::map<string, T *> container);
 }
 
 /*! \brief Rooms interface.

trunk/engine/src/xmlutils.cpp
@@ -1,26 +1,26 @@
 #include "xmlutils.h"
 
-int std::xmlReadInt(TiXmlElement *elem, std::string attribute)
+int xml::xmlReadInt(TiXmlElement *elem, std::string attribute)
 {
     int tmp;
     elem->QueryIntAttribute(attribute.c_str(), &tmp);
     return tmp;
 }
 
-bool std::xmlCheckDoc(TiXmlDocument *doc)
+bool xml::xmlCheckDoc(TiXmlDocument *doc)
 {
     if (doc)
         return doc->LoadFile() &&
-               std::xmlCheckRoot(doc->RootElement()) &&
-               std::xmlCheckImages(doc->RootElement()->FirstChildElement("images")) &&
-               std::xmlCheckVars(doc->RootElement()->FirstChildElement("vars")) &&
-               std::xmlCheckEvents(doc->RootElement()->FirstChildElement("events")) &&
-               std::xmlCheckRooms(doc->RootElement()->FirstChildElement("rooms")) &&
-               std::xmlCheckItems(doc->RootElement()->FirstChildElement("items"));
+               xml::xmlCheckRoot(doc->RootElement()) &&
+               xml::xmlCheckImages(doc->RootElement()->FirstChildElement("images")) &&
+               xml::xmlCheckVars(doc->RootElement()->FirstChildElement("vars")) &&
+               xml::xmlCheckEvents(doc->RootElement()->FirstChildElement("events")) &&
+               xml::xmlCheckRooms(doc->RootElement()->FirstChildElement("rooms")) &&
+               xml::xmlCheckItems(doc->RootElement()->FirstChildElement("items"));
     return false;
 }
 
-bool std::xmlCheckRoot(TiXmlElement *elem)
+bool xml::xmlCheckRoot(TiXmlElement *elem)
 {
     int tmp;
     if (!elem)
@@ -36,7 +36,7 @@ bool std::xmlCheckRoot(TiXmlElement *elem)
     return true;
 }
 
-bool std::xmlCheckImages(TiXmlElement *elem)
+bool xml::xmlCheckImages(TiXmlElement *elem)
 {
     std::set<std::string> ids;
     if (elem)
@@ -53,7 +53,7 @@ bool std::xmlCheckImages(TiXmlElement *elem)
     return true;
 }
 
-bool std::xmlCheckVars(TiXmlElement *elem)
+bool xml::xmlCheckVars(TiXmlElement *elem)
 {
     std::set<std::string> ids;
     if (elem)
@@ -72,7 +72,7 @@ bool std::xmlCheckVars(TiXmlElement *elem)
 }
 
 
-bool std::xmlCheckEvents(TiXmlElement *elem)
+bool xml::xmlCheckEvents(TiXmlElement *elem)
 {
     std::set<std::string> ids;
     if (elem)
@@ -92,7 +92,7 @@ bool std::xmlCheckEvents(TiXmlElement *elem)
     return true;
 }
 
-bool std::xmlCheckActions(TiXmlElement *elem)
+bool xml::xmlCheckActions(TiXmlElement *elem)
 {
     if (elem)
         for (TiXmlElement *i = elem->FirstChildElement("action"); i != 0;
@@ -104,7 +104,7 @@ bool std::xmlCheckActions(TiXmlElement *elem)
     return true;
 }
 
-bool std::xmlCheckParams(TiXmlElement *elem)
+bool xml::xmlCheckParams(TiXmlElement *elem)
 {
     if (elem)
         for (TiXmlElement *i = elem->FirstChildElement("param"); i != 0;
@@ -116,7 +116,7 @@ bool std::xmlCheckParams(TiXmlElement *elem)
     return true;
 }
 
-bool std::xmlCheckReqs(TiXmlElement *elem)
+bool xml::xmlCheckReqs(TiXmlElement *elem)
 {
     if (elem)
     {
@@ -139,7 +139,7 @@ bool std::xmlCheckReqs(TiXmlElement *elem)
     return true;
 }
 
-bool std::xmlCheckRooms(TiXmlElement *elem)
+bool xml::xmlCheckRooms(TiXmlElement *elem)
 {
     std::set<std::string> ids;
     if (elem)
@@ -157,7 +157,7 @@ bool std::xmlCheckRooms(TiXmlElement *elem)
     return true;
 }
 
-bool std::xmlCheckAreas(TiXmlElement *elem)
+bool xml::xmlCheckAreas(TiXmlElement *elem)
 {
     std::set<std::string> ids;
     int tmp;
@@ -180,7 +180,7 @@ bool std::xmlCheckAreas(TiXmlElement *elem)
     return true;
 }
 
-bool std::xmlCheckItems(TiXmlElement *elem)
+bool xml::xmlCheckItems(TiXmlElement *elem)
 {
     std::set<std::string> ids;
     int tmp;
@@ -204,7 +204,7 @@ bool std::xmlCheckItems(TiXmlElement *elem)
     return true;
 }
 
-bool std::xmlCheckDoEvents(TiXmlElement *elem)
+bool xml::xmlCheckDoEvents(TiXmlElement *elem)
 {
     if (elem)
         for (TiXmlElement *i = elem->FirstChildElement("do_event"); i != 0;
@@ -216,7 +216,7 @@ bool std::xmlCheckDoEvents(TiXmlElement *elem)
     return true;
 }
 
-std::vector <TiXmlElement *> std::xmlGetAllChilds(TiXmlElement *elem, std::string id)
+std::vector <TiXmlElement *> xml::xmlGetAllChilds(TiXmlElement *elem, std::string id)
 {
     std::vector <TiXmlElement *> childs;
     if (elem)

trunk/engine/src/xmlutils.h
@@ -7,7 +7,7 @@
 #include <vector> //std::vector
 #include <set> //std::set
 
-namespace std
+namespace xml
 {
     std::vector <TiXmlElement *> xmlGetAllChilds(TiXmlElement *elem, std::string id);
     bool xmlCheckDoc(TiXmlDocument *doc);