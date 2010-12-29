develer-staff/rooms

commit ee1bb155c9a5d8e34d28801ee45997c34b538b7a

Sketching a simple inventory, managed and drawed by frontend.

trunk/engine/frontend/drawdevice.cpp
@@ -19,6 +19,7 @@ DrawDevice::~DrawDevice()
 void DrawDevice::initialize()
 {
     setMouseTracking(true);
+    setFocusPolicy(Qt::StrongFocus);
     std::vector<string> images = engine->getImgNames();
     for (std::vector<string>::iterator i = images.begin();
          i != images.end(); ++i)
@@ -46,25 +47,27 @@ void DrawDevice::quit(int status)
 void DrawDevice::paintEvent(QPaintEvent *event)
 {
     QPainter painter(this);
+    drawRoom(painter);
     switch (engine->state())
     {
         case Engine::GAME:
         {
-            //Draw room
-            Room *room = engine->getRoomsManager()->currentRoom();
-            QPixmap *bg = images[room->bg()];
-            QRect rect(0, 0, width(), height());
-            std::vector <Item *> items = room->items();
-            painter.drawPixmap(rect, *bg);
-            //Draw items
+            painter.drawText(20, height() - 20, item_text.c_str());
+            break;
+        }
+        case Engine::INVENTORY:
+        {
+            std::vector<Item *> items = engine->getInventory();
+            int x = 10;
+            painter.fillRect(0, 0, width(), height(), QColor(0, 0, 0, 128));
             for (std::vector<Item *>::iterator i = items.begin();
                  i != items.end(); ++i)
             {
                 QPixmap *img = images[(*i)->image()];
-                QRect irect((*i)->x(), (*i)->y(), (*i)->w(), (*i)->h());
+                QRect irect(x, 10, 50, 50);
                 painter.drawPixmap(irect, *img);
+                x += 50;
             }
-            painter.drawText(20, height() - 20, item_text.c_str());
             break;
         }
     }
@@ -75,25 +78,68 @@ void DrawDevice::mousePressEvent(QMouseEvent * event)
     engine->click(event->x(), event->y());
     update();
 }
-void DrawDevice::mouseMoveEvent(QMouseEvent *event)
+
+void DrawDevice::keyPressEvent(QKeyEvent * event)
 {
-    Item *item = engine->getRoomsManager()->currentRoom()->itemAt(event->x(),
-                                                                  event->y());
-    Area *area = engine->getRoomsManager()->currentRoom()->areaAt(event->x(),
-                                                                  event->y());
-    if (item != 0)
-        item_text = item->id;
-    else
-        item_text = "";
-
-    if (item != 0)
-        setCursor(Qt::OpenHandCursor);
-    else if (area != 0)
-        setCursor(Qt::PointingHandCursor);
-    else
-        setCursor(Qt::ArrowCursor);
-
-    update();
+    switch(event->key())
+    {
+        case Qt::Key_I:
+        {
+            if (engine->state() == Engine::GAME)
+                engine->setState(Engine::INVENTORY);
+            else
+                engine->setState(Engine::GAME);
+            setCursor(Qt::ArrowCursor);
+            update();
+            break;
+        }
+        default:
+        {
+            QWidget::keyPressEvent(event);
+            break;
+        }
+    }
 }
 
+void DrawDevice::mouseMoveEvent(QMouseEvent *event)
+{
+    if (engine->state() == Engine::GAME)
+    {
+        Item *item = engine->getRoomsManager()->currentRoom()->itemAt(event->x(),
+                                                                      event->y());
+        Area *area = engine->getRoomsManager()->currentRoom()->areaAt(event->x(),
+                                                                      event->y());
+        if (item != 0)
+            item_text = item->id;
+        else
+            item_text = "";
+
+        if (item != 0)
+            setCursor(Qt::OpenHandCursor);
+        else if (area != 0)
+            setCursor(Qt::PointingHandCursor);
+        else
+            setCursor(Qt::ArrowCursor);
+
+        update();
+    }
+}
+
+void DrawDevice::drawRoom(QPainter &painter)
+{
+    //Draw room
+    Room *room = engine->getRoomsManager()->currentRoom();
+    QPixmap *bg = images[room->bg()];
+    QRect rect(0, 0, width(), height());
+    std::vector <Item *> items = room->items();
+    painter.drawPixmap(rect, *bg);
+    //Draw items
+    for (std::vector<Item *>::iterator i = items.begin();
+         i != items.end(); ++i)
+    {
+        QPixmap *img = images[(*i)->image()];
+        QRect irect((*i)->x(), (*i)->y(), (*i)->w(), (*i)->h());
+        painter.drawPixmap(irect, *img);
+    }
+}
 

trunk/engine/frontend/drawdevice.h
@@ -41,6 +41,9 @@ class DrawDevice: public QWidget
         void paintEvent(QPaintEvent *event);
         void mousePressEvent(QMouseEvent * event);
         void mouseMoveEvent(QMouseEvent *event);
+        void keyPressEvent(QKeyEvent *event);
+    protected:
+        void drawRoom(QPainter &painter);
 };
 
 #endif // DRAWDEVICE_H

commit 9e238d2c857254ff52a95c17dca733773c68e52e

Fixing inventory bug introduced in ee1bb155c9a5d8e34d28801ee45997c34b538b7a: exiting from inventory destroyed previous engine state instead of resuming it.

trunk/engine/frontend/drawdevice.cpp
@@ -108,10 +108,13 @@ void DrawDevice::keyPressEvent(QKeyEvent * event)
     {
         case Qt::Key_I:
         {
-            if (engine->state() == Engine::GAME)
+            if (engine->state() != Engine::INVENTORY)
+            {
+                before_inv_state = engine->state();
                 engine->setState(Engine::INVENTORY);
+            }
             else
-                engine->setState(Engine::GAME);
+                engine->setState(before_inv_state);
             setCursor(Qt::ArrowCursor);
             update();
             break;

trunk/engine/frontend/drawdevice.h
@@ -31,6 +31,7 @@ class DrawDevice: public QWidget
         string item_text;
         QListWidget dialog_list;
         QLabel dialog_text;
+        Engine::State before_inv_state;
     public:
         DrawDevice(Engine *eng, QWidget *parent=0);
         ~DrawDevice();