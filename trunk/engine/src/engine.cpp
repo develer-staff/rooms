develer-staff/rooms

commit 03c62d0db0001e14cd8e86bbf130ff791fb5050b

Global refactoring about world creation and world file.

trunk/engine/src/engine.cpp
@@ -101,45 +101,47 @@ bool Engine::loadWorldFromFile(const string filename)
 
 bool Engine::loadWorldFromStr(const string content)
 {
-        TiXmlDocument document;
-        document.Parse(content.c_str());
-        if (!xml::xmlCheckDoc(&document, Engine::VERSION))
-        {
-            logger.write("ERROR: wrong xml document!", Log::ERROR);
-            return false;
-        }
-        TiXmlElement *root = document.RootElement();
-        //Load World attributes
-        logger.write(root->Attribute("name"), Log::NOTE);
-        rooms_mgr->size(xml::xmlReadInt(root, "width"),
-                        xml::xmlReadInt(root, "height"));
-        rooms_mgr->name(root->Attribute("name"));
-        //TODO: manage different screen resolutions
-        //Loading from xml
-        XmlVect images =
-            xml::xmlGetAllChilds(root->FirstChildElement("images"), "img");
-        XmlVect rooms =
-            xml::xmlGetAllChilds(root->FirstChildElement("rooms"), "room");
-        XmlVect events =
-            xml::xmlGetAllChilds(root->FirstChildElement("events"), "event");
-        XmlVect items =
-            xml::xmlGetAllChilds(root->FirstChildElement("items"), "item");
-        XmlVect vars =
-            xml::xmlGetAllChilds(root->FirstChildElement("vars"), "var");
-        XmlVect diags =
-            xml::xmlGetAllChilds(root->FirstChildElement("dialogs"), "dialog");
-        //Populating model
-        createImgsFromXml(images);
-        createEventsFromXml(events);
-        createRoomsFromXml(rooms);
-        createItemsFromXml(items);
-        createVarsFromXml(vars);
-        createDialogsFromXml(diags);
-        //TODO: load rest of world file
-        string start_room = root->Attribute("start");
-        apiRoomGoto(start_room);
-        _state = GAME;
-        return true;
+    RoomsReader reader;
+    if (!reader.loadFromStr(content))
+    {
+        logger.write("ERROR: wrong xml document!", Log::ERROR);
+        return false;
+    }
+    RRNode *node = reader.getCrawler();
+    node->gotoElement("world");
+    string start_room = node->attrStr("start");
+    //Load World attributes
+    logger.write(node->attrStr("name"), Log::NOTE);
+    rooms_mgr->size(node->attrInt("width"), node->attrInt("height"));
+    rooms_mgr->name(node->attrStr("name"));
+    //Loading from xml
+    for (node->gotoElement("images")->gotoChild("img"); node->isNull(); node->gotoNext())
+        images.push_back(node->attrStr("file"));
+    node->gotoRoot();
+
+    for (node->gotoElement("events")->gotoChild("event"); node->isNull(); node->gotoNext())
+        events_mgr->addEvent(node->fetchEvent());
+    node->gotoRoot();
+
+    for (node->gotoElement("rooms")->gotoChild("room"); node->isNull(); node->gotoNext())
+        rooms_mgr->addRoom(node->fetchRoom());
+    node->gotoRoot();
+
+    for (node->gotoElement("items")->gotoChild("item"); node->isNull(); node->gotoNext())
+        rooms_mgr->addItem(node->fetchItem());
+    node->gotoRoot();
+
+    for (node->gotoElement("vars")->gotoChild("var"); node->isNull(); node->gotoNext())
+        events_mgr->setVar(node->attrStr("id"), node->attrInt("value"));
+    node->gotoRoot();
+
+    for (node->gotoElement("dialogs")->gotoChild("dialog"); node->isNull(); node->gotoNext())
+        dialogs[node->attrStr("id")] = node->fetchDialog();
+    node->gotoRoot();
+
+    apiRoomGoto(start_room);
+    setState(GAME);
+    return true;
 }
 
 std::vector<string> Engine::getImgNames() const
@@ -150,101 +152,6 @@ std::vector<string> Engine::getImgNames() const
 std::vector<Item *> Engine::getInventory() const
 {
     return rooms_mgr->room(ROOM_INV)->items();
-}
-
-void Engine::createImgsFromXml(XmlVect imgs)
-{
-    for (XmlVect::iterator i = imgs.begin();
-         i != imgs.end(); ++i)
-        images.push_back((*i)->Attribute("file"));
-}
-
-void Engine::createVarsFromXml(XmlVect vars)
-{
-    for (XmlVect::iterator i = vars.begin();
-         i != vars.end(); ++i)
-        events_mgr->setVar((*i)->Attribute("id"), xml::xmlReadInt((*i), "value"));
-}
-
-void Engine::fillEventFromXml(TiXmlElement *elem, Event *event)
-{
-    //create items parameters
-    XmlVect ireqs = xml::xmlGetAllChilds(elem->FirstChildElement("requirements"), "item_req");
-    for (XmlVect::iterator j = ireqs.begin(); j != ireqs.end(); ++j)
-        event->addItemReq((*j)->Attribute("id"), (*j)->Attribute("value"));
-    //create var parameters
-    XmlVect vreqs = xml::xmlGetAllChilds(elem->FirstChildElement("requirements"), "var_req");
-    for (XmlVect::iterator j = vreqs.begin(); j != vreqs.end(); ++j)
-        event->addVarReq((*j)->Attribute("id"), xml::xmlReadInt((*j), "value"));
-    //create actions
-    XmlVect actions = xml::xmlGetAllChilds(elem->FirstChildElement("actions_if"), "action");
-    for (XmlVect::iterator j = actions.begin(); j != actions.end(); ++j)
-    {
-        Action *act = event->addAction((*j)->Attribute("id"));
-        XmlVect params = xml::xmlGetAllChilds(*j, "param");
-        for (XmlVect::iterator z = params.begin(); z != params.end(); ++z)
-            act->pushParam((*z)->Attribute("value"));
-    }
-}
-
-void Engine::createEventsFromXml(XmlVect events)
-{
-    for (XmlVect::iterator i = events.begin(); i != events.end(); ++i)
-        fillEventFromXml(*i, events_mgr->addEvent((*i)->Attribute("id")));
-}
-
-void Engine::createRoomsFromXml(XmlVect rooms)
-{
-    for (XmlVect::iterator i = rooms.begin(); i != rooms.end(); ++i)
-    {
-        rooms_mgr->addRoom((*i)->Attribute("id"), (*i)->Attribute("bg"));
-        XmlVect areas = xml::xmlGetAllChilds((*i)->FirstChildElement("areas"), "area");
-        for (XmlVect::iterator j = areas.begin(); j != areas.end(); ++j)
-        {
-            rooms_mgr->addArea((*j)->Attribute("id"),
-                               (*i)->Attribute("id"),
-                               xml::xmlReadFloat((*j), "x"),
-                               xml::xmlReadFloat((*j), "y"),
-                               xml::xmlReadFloat((*j), "width"),
-                               xml::xmlReadFloat((*j), "height"),
-                               (*j)->FirstChildElement("do_event")->Attribute("value"));
-        }
-    }
-}
-
-void Engine::createItemsFromXml(XmlVect items)
-{
-    for (XmlVect::iterator i = items.begin(); i != items.end(); ++i)
-    {
-        rooms_mgr->addItem((*i)->Attribute("id"),
-                            (*i)->Attribute("room"),
-                            xml::xmlReadFloat((*i), "x"),
-                            xml::xmlReadFloat((*i), "y"),
-                            xml::xmlReadFloat((*i), "width"),
-                            xml::xmlReadFloat((*i), "height"),
-                            (*i)->FirstChildElement("do_event")->Attribute("value"),
-                            (*i)->Attribute("image"));
-    }
-}
-
-void Engine::createDialogsFromXml(XmlVect diags)
-{
-    for (XmlVect::iterator i = diags.begin(); i != diags.end(); ++i)
-    {
-        Dialog *d = new Dialog((*i)->Attribute("id"), (*i)->Attribute("start"));
-        dialogs[d->id] = d;
-        XmlVect steps = xml::xmlGetAllChilds((*i), "step");
-        for (XmlVect::iterator j = steps.begin(); j != steps.end(); ++j)
-        {
-            DialogStep *step = d->addStep((*j)->Attribute("id"), (*j)->Attribute("text"));
-            fillEventFromXml(*j, step->event);
-            XmlVect links = xml::xmlGetAllChilds((*j), "link");
-            for (XmlVect::iterator z = links.begin(); z != links.end(); ++z)
-            {
-                d->addLink(step->id, (*z)->Attribute("id"), (*z)->Attribute("text"));
-            }
-        }
-    }
 }
 
 RoomsManager *Engine::getRoomsManager() const

trunk/engine/src/roomsreader.cpp
@@ -1,0 +1,399 @@
+#include "roomsreader.h"
+
+RRNode::RRNode(TiXmlElement *root)
+{
+    this->root = root;
+    gotoRoot();
+}
+
+RRNode::~RRNode()
+{
+
+}
+
+RRNode *RRNode::gotoElement(string name)
+{
+    cursor = findElement(root, name);
+}
+
+RRNode *RRNode::gotoRoot()
+{
+    cursor = root;
+    return this;
+}
+
+RRNode *RRNode::gotoChild(string name)
+{
+    cursor = cursor->FirstChildElement(name.c_str());
+    return this;
+}
+
+RRNode *RRNode::gotoParent()
+{
+    cursor = cursor->Parent()->ToElement();
+    return this;
+}
+
+RRNode *RRNode::gotoNext()
+{
+    cursor = cursor->NextSiblingElement(cursor->Value());
+    return this;
+}
+
+bool RRNode::isNull()
+{
+    return cursor;
+}
+
+int RRNode::attrInt(string name)
+{
+    int tmp = 0;
+    cursor->QueryIntAttribute(name.c_str(), &tmp);
+    return tmp;
+}
+
+float RRNode::attrFloat(string name)
+{
+    float tmp = 0.0;
+    cursor->QueryFloatAttribute(name.c_str(), &tmp);
+    return tmp;
+}
+
+string RRNode::attrStr(string name)
+{
+    return cursor->Attribute(name.c_str());
+}
+
+Event *RRNode::fetchEvent()
+{
+    if (isNull()) return 0;
+    Event *event = new Event(attrStr("id"));
+    for (gotoChild("item_req"); !isNull(); gotoNext())
+        event->addItemReq(attrStr("id"), attrStr("value"));
+    gotoParent();
+    for (gotoChild("var_req"); !isNull(); gotoNext())
+        event->addVarReq(attrStr("id"), attrInt("value"));
+    gotoParent();
+    for (gotoChild("action"); !isNull(); gotoNext())
+    {
+        Action *act = event->addAction(attrStr("id"));
+        for (gotoChild("param"); isNull(); gotoNext())
+            act->pushParam(attrStr("value"));
+        gotoParent();
+    }
+    gotoParent();
+    return event;
+}
+
+Room *RRNode::fetchRoom()
+{
+    if (isNull()) return 0;
+    Room *room = new Room(attrStr("id"));
+    room->bg(attrStr("bg"));
+    for (gotoChild("area"); !isNull(); gotoNext())
+    {
+        Area *area = room->addArea(attrStr("id"));
+        area->size(attrFloat("x"), attrFloat("y"), attrFloat("width"), attrFloat("height"));
+        area->setEvent(attrStr("event"));
+    }
+    gotoParent();
+    return room;
+}
+
+Item *RRNode::fetchItem()
+{
+    if (isNull()) return 0;
+    Item *item = new Item(attrStr("id"));
+    item->size(attrFloat("x"), attrFloat("y"), attrFloat("width"), attrFloat("height"));
+    item->setEvent(attrStr("event"));
+    item->setImage(attrStr("image"));
+    item->move(attrStr("room"));
+    return item;
+}
+
+Dialog *RRNode::fetchDialog()
+{
+    if (isNull()) return 0;
+    Dialog *d = new Dialog(attrStr("id"), attrStr("start"));
+    for (gotoChild("step"); !isNull(); gotoNext())
+    {
+        DialogStep *step = d->addStep(attrStr("id"), attrStr("text"));
+        step->event = fetchEvent();
+        for (gotoChild("link"); !isNull(); gotoNext())
+            d->addLink(step->id, attrStr("id"), attrStr("text"));
+         gotoParent();
+    }
+    gotoParent();
+    return d;
+}
+
+TiXmlElement *RRNode::findElement(TiXmlElement *elem, string name)
+{
+    if (elem == 0)
+        return 0;
+    if (string(elem->Value()) == name)
+        return elem;
+    TiXmlElement *result = findElement(elem->NextSiblingElement(), name);
+    if (result != 0)
+        return result;
+    result = findElement(elem->FirstChildElement(), name);
+    if (result != 0)
+        return result;
+    return 0;
+}
+
+RoomsReader::RoomsReader()
+{
+    crawler = 0;
+    parse_map["world"] = &RoomsReader::parseWorld;
+    parse_map["room"] = &RoomsReader::parseRoom;
+    parse_map["area"] = &RoomsReader::parseArea;
+    parse_map["action"] = &RoomsReader::parseAction;
+    parse_map["event"] = &RoomsReader::parseEvent;
+    parse_map["dialog"] = &RoomsReader::parseDialog;
+    parse_map["step"] = &RoomsReader::parseDialogStep;
+    parse_map["var"] = &RoomsReader::parseVar;
+    parse_map["item"] = &RoomsReader::parseItem;
+    parse_map["param"] = &RoomsReader::parseParam;
+    parse_map["item_req"] = &RoomsReader::parseItemReq;
+    parse_map["var_req"] = &RoomsReader::parseVarReq;
+    parse_map["img"] = &RoomsReader::parseImages;
+}
+
+RoomsReader::~RoomsReader()
+{
+    if(crawler)
+        delete crawler;
+}
+
+bool RoomsReader::loadFromFile(const string filename)
+{
+    std::ifstream xml(filename.c_str(), std::ios::binary);
+    xml.seekg (0, std::ios::end);
+    long length = xml.tellg();
+    char *buffer = new char [length];
+    xml.seekg (0, std::ios::beg);
+    xml.read(buffer, length);
+    bool res = loadFromStr(buffer);
+    xml.close();
+    delete [] buffer;
+    return res;
+}
+
+bool RoomsReader::loadFromStr(const string content)
+{
+    doc.Parse(content.c_str());
+    if (!parse()) return false;
+    crawler = new RRNode(doc.RootElement());
+    return true;
+}
+
+bool RoomsReader::parse()
+{
+    return parseElement(doc.RootElement());
+}
+
+RRNode *RoomsReader::getCrawler()
+{
+    return crawler;
+}
+
+bool RoomsReader::checkUniqueId(std::set<string> &ids, const string id)
+{
+    if (ids.count(id) != 0)
+        return false;
+    ids.insert(id);
+    return true;
+}
+
+bool RoomsReader::checkParent(TiXmlElement *elem, string name)
+{
+    return (elem->Parent() != 0 &&
+            string(elem->Parent()->Value()) == name);
+}
+
+bool RoomsReader::parseElement(TiXmlElement *elem)
+{
+    if (elem == 0)
+        return true;
+    bool value = true;
+    ParseMethod method = parse_map.find(elem->Value())->second;
+    if (method)
+        value = (this->*method)(elem);
+    return (value && parseElement(elem->FirstChildElement()) && parseElement(elem->NextSiblingElement()));
+}
+
+bool RoomsReader::parseArea(TiXmlElement *elem)
+{
+    if (!(parseAttr(elem, "id", ATTR_STR) &&
+          parseAttr(elem, "x", ATTR_FLOAT) &&
+          parseAttr(elem, "y", ATTR_FLOAT) &&
+          parseAttr(elem, "width", ATTR_FLOAT) &&
+          parseAttr(elem, "height", ATTR_FLOAT)))
+        return false;
+    if (!(checkUniqueId(unique_ids_areas, elem->Attribute("id")) &&
+        checkParent(elem, "room")))
+        return false;
+    return true;
+}
+
+bool RoomsReader::parseItem(TiXmlElement *elem)
+{
+    if (!(parseAttr(elem, "id", ATTR_STR) &&
+          parseAttr(elem, "room", ATTR_STR) &&
+          parseAttr(elem, "image", ATTR_STR) &&
+          parseAttr(elem, "x", ATTR_FLOAT) &&
+          parseAttr(elem, "y", ATTR_FLOAT) &&
+          parseAttr(elem, "width", ATTR_FLOAT) &&
+          parseAttr(elem, "height", ATTR_FLOAT)))
+        return false;
+    if (!(checkUniqueId(unique_ids_items, elem->Attribute("id")) &&
+        checkParent(elem, "items")))
+        return false;
+    if (checkUniqueId(unique_ids_images, elem->Attribute("image")))
+        return false;
+    return true;
+}
+
+bool RoomsReader::parseRoom(TiXmlElement *elem)
+{
+    if (!(parseAttr(elem, "id", ATTR_STR) &&
+          parseAttr(elem, "bg", ATTR_STR) &&
+          parseAttr(elem, "width", ATTR_INT) &&
+          parseAttr(elem, "height", ATTR_INT)))
+        return false;
+    if (!(checkUniqueId(unique_ids_rooms, elem->Attribute("id")) &&
+        checkParent(elem, "rooms")))
+        return false;
+    return true;
+}
+
+bool RoomsReader::parseAction(TiXmlElement *elem)
+{
+    if (!(parseAttr(elem, "id", ATTR_STR) &&
+          checkParent(elem, "event")))
+        return false;
+    else
+        return true;
+}
+
+bool RoomsReader::parseWorld(TiXmlElement *elem)
+{
+    if (!(parseAttr(elem, "version", ATTR_STR) &&
+          parseAttr(elem, "name", ATTR_STR) &&
+          parseAttr(elem, "start", ATTR_STR) &&
+          parseAttr(elem, "bg", ATTR_STR)))
+        return false;
+    if (checkUniqueId(unique_ids_images, elem->Attribute("bg")))
+        return false;
+    return true;
+}
+
+bool RoomsReader::parseEvent(TiXmlElement *elem)
+{
+    if (!parseAttr(elem, "id", ATTR_STR))
+        return false;
+    if (!(checkUniqueId(unique_ids_events, elem->Attribute("id")) &&
+        checkParent(elem, "events")))
+        return false;
+    return true;
+}
+
+bool RoomsReader::parseVar(TiXmlElement *elem)
+{
+    if (!(parseAttr(elem, "value", ATTR_STR) &&
+        checkParent(elem, "action")))
+        return false;
+    return true;
+}
+
+bool RoomsReader::parseImages(TiXmlElement *elem)
+{
+    if (!(parseAttr(elem, "file", ATTR_STR) &&
+        checkParent(elem, "images")))
+        return false;
+    if (!checkUniqueId(unique_ids_images, elem->Attribute("file")))
+        return false;
+    return true;
+}
+
+
+bool RoomsReader::parseItemReq(TiXmlElement *elem)
+{
+    if (!parseAttr(elem, "id", ATTR_STR))
+        return false;
+    if (!(!checkUniqueId(unique_ids_items, elem->Attribute("id")) &&
+        (checkParent(elem, "event") || checkParent(elem, "step"))))
+        return false;
+    return true;
+}
+
+bool RoomsReader::parseVarReq(TiXmlElement *elem)
+{
+    if (!parseAttr(elem, "id", ATTR_STR))
+        return false;
+    if (!(!checkUniqueId(unique_ids_vars, elem->Attribute("id")) &&
+        (checkParent(elem, "event") || checkParent(elem, "step"))))
+        return false;
+    return true;
+}
+
+bool RoomsReader::parseParam(TiXmlElement *elem)
+{
+    if (!(parseAttr(elem, "id", ATTR_STR) &&
+          parseAttr(elem, "value", ATTR_INT)))
+        return false;
+    if (!(checkUniqueId(unique_ids_vars, elem->Attribute("id")) &&
+        checkParent(elem, "vars")))
+        return false;
+    return true;
+}
+
+bool RoomsReader::parseDialog(TiXmlElement *elem)
+{
+    if (!(parseAttr(elem, "id", ATTR_STR) &&
+          parseAttr(elem, "start", ATTR_STR)))
+        return false;
+    if (!(checkUniqueId(unique_ids_dialogs, elem->Attribute("id")) &&
+        checkParent(elem, "dialogs")))
+        return false;
+    return true;
+}
+
+bool RoomsReader::parseDialogStep(TiXmlElement *elem)
+{
+    if (!(parseAttr(elem, "id", ATTR_STR) &&
+          parseAttr(elem, "text", ATTR_STR) &&
+          checkParent(elem, "dialog")))
+        return false;
+    return true;
+}
+
+bool RoomsReader::parseAttr(TiXmlElement *elem, string name, AttributeType type)
+{
+    switch (type)
+    {
+        case ATTR_FLOAT:
+        {
+            float tmp;
+            return (elem->QueryFloatAttribute(name.c_str(), &tmp) == TIXML_SUCCESS);
+            break;
+        }
+        case ATTR_INT:
+        {
+            int tmp;
+            return (elem->QueryIntAttribute(name.c_str(), &tmp) == TIXML_SUCCESS);
+            break;
+        }
+        case ATTR_STR:
+        {
+            return (elem->Attribute(name.c_str()));
+            break;
+        }
+        default:
+        {
+            return false;
+            break;
+        }
+    }
+}

trunk/engine/engine.pro
@@ -21,7 +21,7 @@ HEADERS += frontend/drawdevice.h \
            src/room.h \
            src/roomsmanager.h \
            src/dialog.h \
-           src/xmlutils.h \
+           src/roomsreader.h \
            lib/tinyxml/tinystr.h \
            lib/tinyxml/tinyxml.h
 SOURCES += frontend/drawdevice.cpp \
@@ -36,7 +36,7 @@ SOURCES += frontend/drawdevice.cpp \
            src/room.cpp \
            src/roomsmanager.cpp \
            src/dialog.cpp \
-           src/xmlutils.cpp \
+           src/roomsreader.cpp \
            lib/tinyxml/tinystr.cpp \
            lib/tinyxml/tinyxml.cpp \
            lib/tinyxml/tinyxmlerror.cpp \

trunk/engine/src/dialog.h
@@ -1,7 +1,9 @@
 #ifndef DIALOG_H
 #define DIALOG_H
 
-#include "engine.h"
+#include "action.h"
+#include "event.h"
+#include "log.h"
 
 #include <string> //std::string
 #include <map> //std::map
@@ -9,7 +11,7 @@
 
 using std::string;
 
-class Engine;
+extern Log logger;
 
 struct DialogStep
 {

trunk/engine/src/engine.h
@@ -10,7 +10,7 @@
 #include "event.h"
 #include "action.h"
 #include "area.h"
-#include "xmlutils.h"
+#include "roomsreader.h"
 
 #include <string> //std::string
 #include <utility> //std::pair
@@ -19,15 +19,6 @@
 
 using std::string;
 
-class Log;
-class RoomsManager;
-class EventsManager;
-class Dialog;
-class Event;
-class Action;
-class Area;
-class Room;
-class Item;
 
 /*! \brief Main class.
  *         It's driven by frontend and it manages game state.
@@ -75,13 +66,6 @@ class Engine
         std::pair<int, int> relToAbsCoord(const float x, const float y);
     private:
         void execActions(const std::vector <Action *> actions);
-        void createImgsFromXml(XmlVect imgs);
-        void fillEventFromXml(TiXmlElement *elem, Event *event);
-        void createEventsFromXml(XmlVect events);
-        void createRoomsFromXml(XmlVect rooms);
-        void createItemsFromXml(XmlVect items);
-        void createVarsFromXml(XmlVect vars);
-        void createDialogsFromXml(XmlVect diags);
         //RISC API
         void apiRoomGoto(const string id);
         void apiVarSet(const string id, const int value);

trunk/engine/src/eventsmanager.cpp
@@ -20,8 +20,13 @@ EventsManager::~EventsManager()
 Event *EventsManager::addEvent(const string name)
 {
     Event *e = new Event(name);
-    events[name] = e;
+    addEvent(e);
     return e;
+}
+
+void EventsManager::addEvent(Event *event)
+{
+    events[event->id] = event;
 }
 
 Event *EventsManager::event(const string name)

trunk/engine/src/eventsmanager.h
@@ -25,6 +25,7 @@ class EventsManager
         ~EventsManager();
     public:
         Event *addEvent(const string name);
+        void addEvent(Event *event);
         Event *event(const string name);
         std::vector <Action *> actionsForEvent(const string name);
         void setVar(const string name, const int value);

trunk/engine/src/roomsmanager.cpp
@@ -65,8 +65,13 @@ Room * RoomsManager::addRoom(const string name, const string bg)
 {
     Room *r = new Room(name);
     r->bg(bg);
-    rooms[name] = r;
+    addRoom(r);
     return r;
+}
+
+void RoomsManager::addRoom(Room *room)
+{
+    rooms[room->id] = room;
 }
 
 Area * RoomsManager::addArea(const string name, const string room, const float x, const float y,
@@ -82,13 +87,18 @@ Item * RoomsManager::addItem(const string name, const string room, const float x
                              const float w, const float h, const string event, const string image)
 {
     Item *i = new Item(name);
-    items[name] = i;
     i->size(x, y, w, h);
     i->setEvent(event);
     i->move(room);
-    rooms[room]->addItem(name, i);
     i->setImage(image);
+    addItem(i);
     return i;
+}
+
+void RoomsManager::addItem(Item *item)
+{
+    items[item->id] = item;
+    rooms[item->parent()]->addItem(item->id, item);
 }
 
 void RoomsManager::moveItem(const string name, const string dest)

commit d7efdd8eb31fdacbe887b895f7b08f4ae54f852c

Fixing bugs introduced in 03c62d0db0001e14cd8e86bbf130ff791fb5050b

trunk/engine/src/engine.cpp
@@ -115,27 +115,28 @@ bool Engine::loadWorldFromStr(const string content)
     rooms_mgr->size(node->attrInt("width"), node->attrInt("height"));
     rooms_mgr->name(node->attrStr("name"));
     //Loading from xml
-    for (node->gotoElement("images")->gotoChild("img"); node->isNull(); node->gotoNext())
+    for (node->gotoElement("images")->gotoChild("img"); !node->isNull(); node->gotoNext())
         images.push_back(node->attrStr("file"));
     node->gotoRoot();
 
-    for (node->gotoElement("events")->gotoChild("event"); node->isNull(); node->gotoNext())
+    for (node->gotoElement("events")->gotoChild("event"); !node->isNull(); node->gotoNext())
         events_mgr->addEvent(node->fetchEvent());
     node->gotoRoot();
 
-    for (node->gotoElement("rooms")->gotoChild("room"); node->isNull(); node->gotoNext())
+    for (node->gotoElement("rooms")->gotoChild("room"); !node->isNull(); node->gotoNext())
         rooms_mgr->addRoom(node->fetchRoom());
     node->gotoRoot();
 
-    for (node->gotoElement("items")->gotoChild("item"); node->isNull(); node->gotoNext())
+    for (node->gotoElement("items")->gotoChild("item"); !node->isNull(); node->gotoNext())
         rooms_mgr->addItem(node->fetchItem());
+        node->isNull();
     node->gotoRoot();
 
-    for (node->gotoElement("vars")->gotoChild("var"); node->isNull(); node->gotoNext())
+    for (node->gotoElement("vars")->gotoChild("var"); !node->isNull(); node->gotoNext())
         events_mgr->setVar(node->attrStr("id"), node->attrInt("value"));
     node->gotoRoot();
 
-    for (node->gotoElement("dialogs")->gotoChild("dialog"); node->isNull(); node->gotoNext())
+    for (node->gotoElement("dialogs")->gotoChild("dialog"); !node->isNull(); node->gotoNext())
         dialogs[node->attrStr("id")] = node->fetchDialog();
     node->gotoRoot();
 

trunk/engine/src/roomsreader.cpp
@@ -14,6 +14,7 @@ RRNode::~RRNode()
 RRNode *RRNode::gotoElement(string name)
 {
     cursor = findElement(root, name);
+    return this;
 }
 
 RRNode *RRNode::gotoRoot()
@@ -24,25 +25,29 @@ RRNode *RRNode::gotoRoot()
 
 RRNode *RRNode::gotoChild(string name)
 {
-    cursor = cursor->FirstChildElement(name.c_str());
+    if(!isNull())
+        cursor = cursor->FirstChildElement(name.c_str());
     return this;
 }
 
 RRNode *RRNode::gotoParent()
 {
-    cursor = cursor->Parent()->ToElement();
+    if(!isNull())
+        cursor = cursor->Parent()->ToElement();
     return this;
 }
 
 RRNode *RRNode::gotoNext()
 {
-    cursor = cursor->NextSiblingElement(cursor->Value());
+    if(!isNull())
+        cursor = cursor->NextSiblingElement(cursor->Value());
     return this;
 }
 
 bool RRNode::isNull()
 {
-    return cursor;
+    bool result = cursor;
+    return !result;
 }
 
 int RRNode::attrInt(string name)
@@ -77,7 +82,7 @@ Event *RRNode::fetchEvent()
     for (gotoChild("action"); !isNull(); gotoNext())
     {
         Action *act = event->addAction(attrStr("id"));
-        for (gotoChild("param"); isNull(); gotoNext())
+        for (gotoChild("param"); !isNull(); gotoNext())
             act->pushParam(attrStr("value"));
         gotoParent();
     }