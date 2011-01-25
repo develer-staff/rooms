develer-staff/rooms

commit 6be5a73131c793ed6180f473aa005755044c12da

Divided parsing from loading in RoomsReader. When we load a saved game, it has a different format from world file.

trunk/engine/src/roomsreader.cpp
@@ -268,18 +268,6 @@ bool RoomsReader::loadFromStr(const string content)
 {
     doc = new TiXmlDocument;
     doc->Parse(content.c_str());
-    if (!parse())
-    {
-        delete doc;
-        doc = 0;
-        if (file_version < VERSION)
-        {
-            logger.write("Old version detected: " + floatToStr(file_version) + ". Upgrading...", Log::WARNING);
-            string new_content = upgrade(content);
-            return loadFromStr(new_content);
-        }
-        return false;
-    }
     crawler = new RRNode(doc->RootElement());
     return true;
 }
@@ -315,7 +303,21 @@ string RoomsReader::upgrade(string content)
 
 bool RoomsReader::parse()
 {
-    return parseElement(doc->RootElement());
+    if (!parseElement(doc->RootElement()))
+    {
+        string content = doc->RootElement()->GetText();
+        delete doc;
+        doc = 0;
+        if (file_version < VERSION)
+        {
+            logger.write("Old version detected: " + floatToStr(file_version) + ". Upgrading...", Log::WARNING);
+            string new_content = upgrade(content);
+            loadFromStr(new_content);
+            return parse();
+        }
+        return false;
+    }
+    return true;
 }
 
 RRNode *RoomsReader::getCrawler()

trunk/engine/src/roomsreader.h
@@ -170,6 +170,8 @@ class RoomsReader
          * \return          World data upgraded.
          */
         string upgrade(string old_content);
+        /// Parses the file and returns true if it's a correct world file.
+        bool parse();
         /// Creates an empty document
         void loadEmptyDoc();
         /// Obtain crawler pointer, to navigate inside world file.
@@ -190,7 +192,6 @@ class RoomsReader
         std::set<string> unique_ids_images;
         std::set<string> unique_ids_dialogs;
         int file_version; // initialized with parse(), modified in upgrade()
-        bool parse();
         bool parseElement(TiXmlElement *elem);
         bool parseWorld(TiXmlElement *elem);
         bool parseRoom(TiXmlElement *elem);

trunk/engine/src/engine.cpp
@@ -188,7 +188,7 @@ bool Engine::loadWorldFromFile(const string filename)
 bool Engine::loadWorldFromStr(const string content)
 {
     RoomsReader reader;
-    if (!reader.loadFromStr(content))
+    if (!reader.loadFromStr(content) || !reader.parse())
     {
         logger.write("ERROR: wrong xml document!", Log::ERROR);
         return false;

commit 793475dcc4d2da7e3cec317656c3a9cf8cbaa504

Fixing upgrade problem introduced in 6be5a73131c793ed6180f473aa005755044c12da: RootElement()->GetText(); doesnt work, returns always NULL. Using std::string instead.

trunk/engine/src/roomsreader.cpp
@@ -269,6 +269,7 @@ bool RoomsReader::loadFromStr(const string content)
     doc = new TiXmlDocument;
     doc->Parse(content.c_str());
     crawler = new RRNode(doc->RootElement());
+    file_content = content;
     return true;
 }
 
@@ -298,6 +299,7 @@ string RoomsReader::upgrade(string content)
         content = upgrade_funcs[i - 1](content);
     }
     logger.write("Upgrade successful!", Log::WARNING);
+    file_content = content;
     return content;
 }
 
@@ -305,13 +307,12 @@ bool RoomsReader::parse()
 {
     if (!parseElement(doc->RootElement()))
     {
-        string content = doc->RootElement()->GetText();
         delete doc;
         doc = 0;
         if (file_version < VERSION)
         {
             logger.write("Old version detected: " + floatToStr(file_version) + ". Upgrading...", Log::WARNING);
-            string new_content = upgrade(content);
+            string new_content = upgrade(file_content);
             loadFromStr(new_content);
             return parse();
         }

trunk/engine/src/roomsreader.h
@@ -192,6 +192,7 @@ class RoomsReader
         std::set<string> unique_ids_images;
         std::set<string> unique_ids_dialogs;
         int file_version; // initialized with parse(), modified in upgrade()
+        string file_content; // string filled with file content
         bool parseElement(TiXmlElement *elem);
         bool parseWorld(TiXmlElement *elem);
         bool parseRoom(TiXmlElement *elem);