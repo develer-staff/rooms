2d7bd39b013862a8da5e3d4046ee88e32dd67b33
Daniele Maccioni <gendoikari@develer.com>
2011-01-28 17:56:07 +0100

    Style: tab and space fix

--- trunk/engine/src/roomsmanager.cpp
+++ trunk/engine/src/roomsmanager.cpp
@@ -4,165 +4,165 @@
 #include "item.h"
 #include "area.h"
 
-template <class T> void utils::freeElements(std::map<string , T *> &container)
+template <class T> void utils::freeElements(std::map<string, T *> &container)
 {
-    for (typename std::map<string, T *>::iterator i = container.begin();
-         i != container.end(); ++i)
-    {
-        logger.write("Garbage collector: " + i->second->id, Log::NOTE);
-        delete i->second;
-    }
-    container.clear();
+	for (typename std::map<string, T *>::iterator i = container.begin();
+			i != container.end(); ++i)
+	{
+		logger.write("Garbage collector: " + i->second->id, Log::NOTE);
+		delete i->second;
+	}
+	container.clear();
 }
 
 template <class T> T *utils::getElement(string name, std::map<string, T *> container)
 {
-    typename std::map<string, T *>::iterator i = container.find(name);
-    if (i == container.end())
-        return 0;
-    else
-        return i->second;
+	typename std::map<string, T *>::iterator i = container.find(name);
+	if (i == container.end())
+		return 0;
+	else
+		return i->second;
 }
 
 RoomsManager::RoomsManager()
 {
-    current_room = 0;
+	current_room = 0;
 }
 
 RoomsManager::~RoomsManager()
 {
-    utils::freeElements(rooms);
-    utils::freeElements(items);
+	utils::freeElements(rooms);
+	utils::freeElements(items);
 }
 
 void RoomsManager::setWorldName(const string name)
 {
-    _name = name;
+	_name = name;
 }
 
 string RoomsManager::worldName() const
 {
-    return _name;
+	return _name;
 }
 
 void RoomsManager::setRoomSize(const int width, const int height)
 {
-    height > 10 ? _height = height : _height = 10;
-    width > 10 ? _width = width : _width = 10;
+	height > 10 ? _height = height : _height = 10;
+	width > 10 ? _width = width : _width = 10;
 }
 
 int RoomsManager::width() const
 {
-    return _width;
+	return _width;
 }
 
 int RoomsManager::height() const
 {
-    return _height;
+	return _height;
 }
 
 Room * RoomsManager::addRoom(const string name, const string bg)
 {
-    Room *r = new Room(name);
-    r->bg(bg);
-    addRoom(r);
-    return r;
+	Room *r = new Room(name);
+	r->bg(bg);
+	addRoom(r);
+	return r;
 }
 
 void RoomsManager::addRoom(Room *room)
 {
-    rooms[room->id] = room;
+	rooms[room->id] = room;
 }
 
 Area * RoomsManager::addArea(const string name, const string room, const float x, const float y,
-                             const float w, const float h, const string event)
+							 const float w, const float h, const string event)
 {
-    Area *a = this->room(room)->addArea(name);
-    a->setSize(x, y, w, h);
-    a->setEvent(event);
-    return a;
+	Area *a = this->room(room)->addArea(name);
+	a->setSize(x, y, w, h);
+	a->setEvent(event);
+	return a;
 }
 
 Item * RoomsManager::addItem(const string name, const string room, const float x, const float y,
-                             const float w, const float h, const string event, const string image)
+							 const float w, const float h, const string event, const string image)
 {
-    Item *i = new Item(name);
-    i->setSize(x, y, w, h);
-    i->setEvent(event);
-    i->move(room);
-    i->setImage(image);
-    addItem(i);
-    return i;
+	Item *i = new Item(name);
+	i->setSize(x, y, w, h);
+	i->setEvent(event);
+	i->move(room);
+	i->setImage(image);
+	addItem(i);
+	return i;
 }
 
 void RoomsManager::addItem(Item *item)
 {
-    items[item->id] = item;
-    rooms[item->parent()]->addItem(item->id, item);
+	items[item->id] = item;
+	rooms[item->parent()]->addItem(item->id, item);
 }
 
 void RoomsManager::moveItem(const string name, const string dest)
 {
-    Item *mov_item = item(name);
-    if (mov_item != 0)
-    {
-        string parent = mov_item->parent();
-        Room *r_parent = room(parent);
-        Room *r_dest = room(dest);
-        if (r_parent == 0 || r_dest == 0)
-        {
-            logger.write("WARNING: invalid item movement!", Log::WARNING);
-            return;
-        }
-        r_parent->remItem(name);
-        mov_item->move(dest);
-        r_dest->addItem(name, mov_item);
-    }
+	Item *mov_item = item(name);
+	if (mov_item != 0)
+	{
+		string parent = mov_item->parent();
+		Room *r_parent = room(parent);
+		Room *r_dest = room(dest);
+		if (r_parent == 0 || r_dest == 0)
+		{
+			logger.write("WARNING: invalid item movement!", Log::WARNING);
+			return;
+		}
+		r_parent->remItem(name);
+		mov_item->move(dest);
+		r_dest->addItem(name, mov_item);
+	}
 }
 
 Room * RoomsManager::room(const string name)
 {
-    return utils::getElement(name, rooms);
+	return utils::getElement(name, rooms);
 }
 
 Area * RoomsManager::area(const string name)
 {
-    return current_room->area(name);
+	return current_room->area(name);
 }
 
 Item * RoomsManager::item(const string name)
 {
-    return utils::getElement(name, items);
+	return utils::getElement(name, items);
 }
 
 std::map <string, Item *> RoomsManager::getItems()
 {
-    return items;
+	return items;
 }
 
 void RoomsManager::setCurrentRoom(const string name)
 {
-    current_room = room(name);
+	current_room = room(name);
 }
 
 Room *RoomsManager::currentRoom()
 {
-    return current_room;
+	return current_room;
 }
 
 string RoomsManager::eventAt(const float x, const float y)
 {
-    return current_room->eventAt(x, y);
+	return current_room->eventAt(x, y);
 }
 
 bool RoomsManager::checkItemPlace(const std::vector <std::pair <string, string> > reqs)
 {
-    for (std::vector <std::pair <string, string> >::const_iterator i = reqs.begin();
-         i != reqs.end(); ++i)
-    {
-        if (item(i->first)->parent() != i->second)
-            return false;
-    }
-    return true;
+	for (std::vector <std::pair <string, string> >::const_iterator i = reqs.begin();
+			i != reqs.end(); ++i)
+	{
+		if (item(i->first)->parent() != i->second)
+			return false;
+	}
+	return true;
 }
 
--- trunk/engine/src/roomsmanager.h
+++ trunk/engine/src/roomsmanager.h
@@ -28,8 +28,8 @@ using std::string;
 
 namespace utils
 {
-    template <class T> void freeElements(std::map<string , T *> &container);
-    template <class T> T *getElement(string name, std::map<string, T *> container);
+	template <class T> void freeElements(std::map<string, T *> &container);
+	template <class T> T *getElement(string name, std::map<string, T *> container);
 }
 
 /** \brief The RoomsManager class handles rooms.
@@ -38,57 +38,57 @@ namespace utils
  */
 class RoomsManager
 {
-    private:
-        std::map <string, Room *> rooms;
-        std::map <string, Item *> items;
-        string _name;
-        int _height;
-        int _width;
-        Room *current_room;
-    public:
-        RoomsManager();
-        ~RoomsManager();
-    public:
-        /// Creates a Room with a given id and a given background.
-        Room *addRoom(const string name, const string bg);
-        /// Adds a created Room to the internal rooms list.
-        void addRoom(Room *room);
-        /// Creates an Area and initializes it with given values.
-        Area *addArea(const string name, const string room, const float x, const float y,
-                      const float w, const float h, const string event);
-        /// Creates an Item and initializes it with given values.
-        Item *addItem(const string name, const string room, const float x, const float y,
-                      const float w, const float h, const string event, const string image);
-        /// Adds a created Item to the internal item list.
-        void addItem(Item *item);
-        /// Returns a room with given id.
-        Room *room(const string name);
-        /// Returns an area with given id.
-        Area *area(const string name);
-        /// Returns an item with given id.
-        Item *item(const string name);
-        /// Returns items list
-        std::map <string, Item *> getItems();
-        /// Sets world/game name.
-        void setWorldName(const string name);
-        /// Gets the world/game name.
-        string worldName() const;
-        /// Sets the game rooms size.
-        void setRoomSize(const int width, const int height);
-        /// Gets rooms absolute height.
-        int height() const;
-        /// Gets rooms absolute width.
-        int width() const;
-        /// Sets where player is at the moment.
-        void setCurrentRoom(const string name);
-        /// Gets the current room.
-        Room *currentRoom();
-        /// Gets the event id under a point, if it exists. It returns "" otherwise.
-        string eventAt(const float x, const float y);
-        /// Moves the given item to the given room. It updates both source room and destination room.
-        void moveItem(const string name, const string dest);
-        /// Checks if a list of item requirements is satisfied.
-        bool checkItemPlace(const std::vector <std::pair <string, string> > reqs);
+private:
+	std::map <string, Room *> rooms;
+	std::map <string, Item *> items;
+	string _name;
+	int _height;
+	int _width;
+	Room *current_room;
+public:
+	RoomsManager();
+	~RoomsManager();
+public:
+	/// Creates a Room with a given id and a given background.
+	Room *addRoom(const string name, const string bg);
+	/// Adds a created Room to the internal rooms list.
+	void addRoom(Room *room);
+	/// Creates an Area and initializes it with given values.
+	Area *addArea(const string name, const string room, const float x, const float y,
+				  const float w, const float h, const string event);
+	/// Creates an Item and initializes it with given values.
+	Item *addItem(const string name, const string room, const float x, const float y,
+				  const float w, const float h, const string event, const string image);
+	/// Adds a created Item to the internal item list.
+	void addItem(Item *item);
+	/// Returns a room with given id.
+	Room *room(const string name);
+	/// Returns an area with given id.
+	Area *area(const string name);
+	/// Returns an item with given id.
+	Item *item(const string name);
+	/// Returns items list
+	std::map <string, Item *> getItems();
+	/// Sets world/game name.
+	void setWorldName(const string name);
+	/// Gets the world/game name.
+	string worldName() const;
+	/// Sets the game rooms size.
+	void setRoomSize(const int width, const int height);
+	/// Gets rooms absolute height.
+	int height() const;
+	/// Gets rooms absolute width.
+	int width() const;
+	/// Sets where player is at the moment.
+	void setCurrentRoom(const string name);
+	/// Gets the current room.
+	Room *currentRoom();
+	/// Gets the event id under a point, if it exists. It returns "" otherwise.
+	string eventAt(const float x, const float y);
+	/// Moves the given item to the given room. It updates both source room and destination room.
+	void moveItem(const string name, const string dest);
+	/// Checks if a list of item requirements is satisfied.
+	bool checkItemPlace(const std::vector <std::pair <string, string> > reqs);
 };
 
 
