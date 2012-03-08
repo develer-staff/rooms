948cc129bb05c36814b5d99d98570662b9a4d6b3
Matteo Baracani <bara@develer.com>
2012-03-08 11:39:49 +0100

    Move room_selected_name assignment line in editor.py

--- trunk/editor/editor.py
+++ trunk/editor/editor.py
@@ -5,15 +5,15 @@ from PyQt4.QtGui import *
 
 from roomEditor import RoomEditor
 import sys
-from room import Room
+from structData.room import Room
 from roomManager import RoomManager
 from xml.etree import ElementTree
-from area import Area
-from request import Request
-from action import Action
-from param import Param
-from item import Item
-from event import Event
+from structData.area import Area
+from structData.request import Request
+from structData.action import Action
+from structData.param import Param
+from structData.item import Item
+from structData.event import Event
 from xml.dom import minidom
 from openFileRooms import openFileRooms
 from saveFileRooms import saveFileRooms
@@ -33,20 +33,22 @@ class Editor(QWidget):
         file_open = QFileDialog()
         self.path_file = file_open.getOpenFileName(filter="*.rooms")
         self.informations, self.rooms, self.events, self.items, self.images = openFileRooms(self.path_file)
+        self.selected_room_name = "Locanda"
         room_editor = RoomEditor(self)
         room_manager = RoomManager(self)
+
         horizontal.addWidget(room_manager)
         horizontal.addWidget(room_editor)
-        self.selected_room_name = "Locanda"
+
         self.connect(room_manager, SIGNAL("pressButton"), self.changeRoom)
         self.changeRoom(self.selected_room_name)
         file_open = QFileDialog()
         self.path_file = ""
-        self.path_file = file_open.getSaveFileNameAndFilter(parent=self,
-                                                            filter="*.rooms")
-        if self.path_file:
-            saveFileRooms(self.path_file, self.rooms, self.events,
-                          self.items, self.informations, self.images)
+        #self.path_file = file_open.getSaveFileNameAndFilter(parent=self,
+                                                          #  filter = "*.rooms")
+        #if self.path_file:
+         #   saveFileRooms(self.path_file, self.rooms, self.events,
+          #                self.items, self.informations, self.images)
 
     def searchEvent(self, id):
         for event in self.events:
