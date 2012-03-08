5f162d96c1ab6ee4b04f38153915ef1f437c1216
Matteo Baracani <bara@develer.com>
2012-03-08 11:22:23 +0100

    Add room image visualization in roomManager.py

--- trunk/editor/roomManager.py
+++ trunk/editor/roomManager.py
@@ -2,13 +2,39 @@
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from structData.room import Room
+
+class RoomList(QWidget):
+
+    def __init__(self, parent=None):
+        super(RoomList, self).__init__(parent)
+        vertical = QVBoxLayout(self)
+        for key in parent.rooms.keys():
+            self.scroll_area = QScrollArea(self)
+            self.scroll_area.setMinimumSize(260, 260)
+            self.scroll_area.setSizePolicy(QSizePolicy.Fixed,
+                                           QSizePolicy.Fixed)
+            #self.scroll_area.setBackgroundRole(QPalette.Dark)
+            self.scroll_area.setAlignment(Qt.AlignCenter)
+            self.image_label = QLabel()
+            image = QImage(parent.rooms[key].bg).scaled(250, 250)
+            self.image_label.setPixmap(QPixmap.fromImage(image))
+            self.scroll_area.setWidget(self.image_label)
+            vertical.addWidget(self.scroll_area, Qt.AlignCenter)
 
 class RoomManager(QWidget):
 
     def __init__(self, parent=None):
         super(RoomManager, self).__init__(parent)
-        self.bottone1 = QPushButton("Locanda")
-        self.bottone2 = QPushButton("Stanza Centrale")
-        vertical = QVBoxLayout(self)
-        vertical.addWidget(self.bottone1)
-        vertical.addWidget(self.bottone2)
+        self.setMinimumSize(300, 1000)
+        self.setSizePolicy(QSizePolicy(QSizePolicy.Fixed,
+                                       QSizePolicy.Fixed))
+        vertical_scroll = QScrollArea(self)
+        vertical_scroll.setVerticalScrollBarPolicy(Qt.ScrollBarAsNeeded)
+        vertical_scroll.setWidgetResizable(False)
+        vertical_scroll.setMinimumSize(300, 1000)
+        vertical_scroll.setAlignment(Qt.AlignVCenter)
+        my_widget = RoomList(parent)
+        #vertical_scroll.setAlignment(Qt.Vertical)
+        vertical_scroll.setWidget(my_widget)
+            #verticalLayout.addWidget(self.scroll_area)
