1e55211f2c952a684eb774d11dc01f3dd6173d98
Matteo Baracani <bara@develer.com>
2012-03-09 11:59:32 +0100

    removed attribute set_value

--- trunk/editor/structData/var.py
+++ trunk/editor/structData/var.py
@@ -2,7 +2,6 @@
 
 class Var(object):
 
-    def __init__(self, name, start_value, set_value=None):
+    def __init__(self, name, start_value):
         self.name = name
         self.start_value = start_value
-        self.set_value = set_value
