4f795edc5c536201875c99c1fa8d4f6c3acd33ec
Matteo Baracani <bara@develer.com>
2012-03-12 10:58:44 +0100

    add signal currentRoomChanged(const QString &) to notify romm selection changed

--- trunk/editor/roomManager.py
+++ trunk/editor/roomManager.py
@@ -22,23 +22,22 @@ class RoomManager(QWidget):
         self.rooms_list = QListWidget(self)
         self.rooms_list.setMinimumSize(300, 1000)
         self.rooms_list.setIconSize(QSize(150, 150))
-        for key in g_world.rooms:
-            image = QImage(g_world.rooms[key].bg)
-            room_item = QListWidgetItem(QIcon(QPixmap.fromImage(image)), g_world.rooms[key].id)
+        self.selected_room = g_world.informations.start
+        for key, value in g_world.rooms.items():
+            image = QImage(value.bg)
+            room_item = QListWidgetItem(QIcon(QPixmap.fromImage(image)),
+                                        value.id)
             self.rooms_list.addItem(room_item)
+            if value.id == self.selected_room:
+                room_item.setSelected(True)
+
         #vertical_scroll.setAlignment(Qt.Vertical)
         #self.vertical_scroll.setWidget(rooms_list)
-        self.connect(self.rooms_list, SIGNAL("itemSelectionChanged()"),
-                     self.changeItemSelected)
+        self.connect(self.rooms_list,
+                     SIGNAL("currentTextChanged(const QString &)"),
+                     SIGNAL("currentRoomChanged(const QString &)"))
 
     def setRoomSelected(self, room_name):
         item = self.rooms_list.findItems(room_name, Qt.MatchFixedString)
         if item:
             item[0].setSelected(True)
-
-    def changeItemSelected(self):
-        item = self.rooms_list.selectedItems()
-        if item:
-            print item[0].text()
-            self.emit(SIGNAL("changeRoomSelected"), item[0].text())
-
