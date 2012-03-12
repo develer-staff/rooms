085877c538f26533d7b4bbcc97e809553f766caa
Matteo Baracani <bara@develer.com>
2012-03-12 12:07:55 +0100

    add class Subject to implement Observer design pattern on data

---
+++ trunk/editor/structData/subject.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+class Subject(object):
+
+
+    def __init__(self):
+        self.observers = []
+
+    def subscrive(self, observer):
+        self.observers.append(observer)
+
+    def unsubscribe(self, observer):
+        self.observers.remove(observer)
+
+    def notify(self):
+        for observer in self.observers:
+            observer.update()
