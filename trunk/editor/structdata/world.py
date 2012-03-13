2c8983dc8b4ff1ea50da14cd08da827634b081ee
Matteo Baracani <bara@develer.com>
2012-03-13 11:43:43 +0100

    rename in package structdata information.py in world.py

--- trunk/editor/structdata/world.py
+++ trunk/editor/structdata/world.py
@@ -1,25 +1,15 @@
 #!/usr/bin/env python
 
-try:
-    from collections import OrderedDict
-except ImportError:
-    from misc.dict import OrderedDict
-
 from origin import OriginData
-from subject import Subject
 
-class World(OriginData, Subject):
-    tag_name = "world"
-    def __init__(self):
-        super(OriginData, self).__init__()
-        super(Subject, self).__init__()
-        self.informations = None
-        self.images = {}
-        self.items = OrderedDict()
-        self.vars = {}
-        self.events = OrderedDict()
-        self.rooms = OrderedDict()
-        self.selected_room = None
+class World(OriginData):
 
-g_world = World()
+    tag_name = 'world'
 
+    def __init__(self, version, name, width, height, start):
+
+        self.version = version
+        self.name = name
+        self.width = width
+        self.height = height
+        self.start = start
