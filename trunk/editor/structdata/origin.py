b22a86476b22f4f1c318488bebc2ed17287e08ff
Matteo Baracani <bara@develer.com>
2012-03-13 14:51:02 +0100

    clean whitespace

--- trunk/editor/structdata/origin.py
+++ trunk/editor/structdata/origin.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 
 class OriginData(object):
-"""
-classe base da cui derivano tutte le classi del modello. Questa dipendenza
-e' fatta per la serializzazione nel momento del salvataggio.
-I dati che vengono serializzati sono tutti gli attributi pubblici che non 
-cominciano con underscore e hanno nome uguale ai tag che vengono salvati 
-nel file XML nel file .rooms
-Gli attributi di tipo lista verranno poi serializzati nuovamente
-"""
+    """
+    classe base da cui derivano tutte le classi del modello. Questa dipendenza
+    e' fatta per la serializzazione nel momento del salvataggio.
+    I dati che vengono serializzati sono tutti gli attributi pubblici che non 
+    cominciano con underscore e hanno nome uguale ai tag che vengono salvati 
+    nel file XML nel file .rooms
+    Gli attributi di tipo lista verranno poi serializzati nuovamente
+    """
 
     def dictionary(self):
         attributes_dictionary = {}
