dce5d69eb28e6e646c3381f4096d418b5b7bae7a
Matteo Baracani <bara@develer.com>
2012-03-13 12:51:41 +0100

    update to use g_project structure, add setRoom function to show the current room

--- trunk/editor/roomeditor.py
+++ trunk/editor/roomeditor.py
@@ -6,12 +6,13 @@ from xml.etree.ElementTree import ElementTree
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
 
-from structdata.world import g_world
+from structdata.project import g_project
 
 class RoomEditor(QWidget):
 
     def __init__(self, room=None, parent=None):
         super(RoomEditor, self).__init__(parent)
+        self.room_name = room.id if room else ""
         self.setSizePolicy(QSizePolicy(QSizePolicy.Expanding,
                                        QSizePolicy.Expanding))
         self.setMinimumSize(1000, 1000)
@@ -23,32 +24,37 @@ class RoomEditor(QWidget):
         self.scroll_area.setBackgroundRole(QPalette.Dark)
         self.scroll_area.setAlignment(Qt.AlignCenter)
         self.change_room_name = QLineEdit(self)
-        self.change_room_name.setText(g_world.selected_room)
         self.change_room_name.setAlignment(Qt.AlignCenter)
         self.change_room_name.move(self.scroll_area.height() / 2,
                                    self.scroll_area.width())
-        image = QPixmap(g_world.rooms[g_world.selected_room].bg)
-        self.label.setPixmap(image)
+        self.setRoom(room)
         self.scroll_area.setWidget(self.label)
         self.connect(self.change_room_name,
                      SIGNAL("textEdited(const QString &)"),
                      self.setRoomName)
 
     def setRoomName(self, new_room_name):
-        old_room = g_world.selected_room
-        if g_world.informations.start == g_world.selected_room:
-            g_world.informations.start = str(new_room_name)
-        g_world.selected_room = str(new_room_name)
-        room = g_world.rooms[old_room]
+        old_room = self.room_name
+        if g_project.data['world'].start == old_room:
+            g_project.data['world'].start = str(new_room_name)
+        room = g_project.data['rooms'][old_room]
         room.id = str(new_room_name)
-        del g_world.rooms[old_room]
-        g_world.rooms[room.id] = room
-        g_world.notify()
+        del g_project.data['rooms'][old_room]
+        g_project.data['rooms'][room.id] = room
+        g_project.notify()
+
+    def setRoom(self, room):
+        if room:
+            self.room_name = room.id
+            self.label.setPixmap(QPixmap(room.bg))
+            self.change_room_name.setText(room.id)
 
     def changeCurrentRoom(self, room_id):
-        self.room = str(room_id)
-        self.label.setPixmap(QPixmap(g_world.rooms[self.room].bg))
-        self.change_room_name.setText(room_id)
+        self.room_name = str(room_id)
+        if self.room_name:
+            self.label.setPixmap(QPixmap(g_project.data['rooms']\
+                                         [self.room_name].bg))
+            self.change_room_name.setText(room_id)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
