e8db9cd21163bbec0a4f4747bf5398b9bd9e2199
Matteo Baracani <bara@develer.com>
2012-03-13 12:57:07 +0100

    add variable to store room selected, change contextMenuEvent to select the start room, change function name update in update_data

--- trunk/editor/roommanager.py
+++ trunk/editor/roommanager.py
@@ -5,13 +5,14 @@ from PyQt4.QtGui import *
 
 
 from structdata.room import Room
-from structdata.world import g_world
+from structdata.project import g_project
 
 class RoomManager(QWidget):
 
-    def __init__(self, parent=None):
+    def __init__(self, selected_room=None, parent=None):
         super(RoomManager, self).__init__(parent)
-        g_world.subscribe(self)
+        self.selected_room = selected_room
+        g_project.subscribe(self)
         self.setMinimumSize(300, 1000)
         self.setSizePolicy(QSizePolicy(QSizePolicy.Fixed,
                                        QSizePolicy.Fixed))
@@ -23,15 +24,14 @@ class RoomManager(QWidget):
         self.rooms_list = QListWidget(self)
         self.rooms_list.setMinimumSize(300, 1000)
         self.rooms_list.setIconSize(QSize(150, 150))
-        g_world.selected_room = g_world.informations.start
         self.rooms_list.setAlternatingRowColors(True)
-        for key, value in g_world.rooms.items():
+        for key, value in g_project.data['rooms'].items():
             image = QImage(value.bg)
             room_item = QListWidgetItem(QIcon(QPixmap.fromImage(image)),
                                         value.id)
             self.rooms_list.addItem(room_item)
             room_item.setForeground(Qt.black)
-            if value.id == g_world.selected_room:
+            if value.id == self.selected_room.id:
                 room_item.setSelected(True)
                 room_item.setBackgroundColor(Qt.yellow)
         #vertical_scroll.setAlignment(Qt.Vertical)
@@ -41,30 +41,25 @@ class RoomManager(QWidget):
                      SIGNAL("currentRoomChanged(const QString &)"))
 
     def contextMenuEvent(self, event):
+        self.rooms_list.findItems(self.selected_room.id,
+                                  Qt.MatchFixedString)[0].setBackground(Qt.white)
         selected_item = self.rooms_list.selectedItems()[0]
-        if g_world.selected_room != selected_item.text():
-            selected_item.setBackground(Qt.yellow)
-            self.rooms_list.findItems(g_world.selected_room,
-                                      Qt.MatchFixedString)[0].setBackground(Qt.white)
-        else:
-            list_color = [Qt.white, Qt.yellow]
-            color = list_color[(list_color.index(selected_item.background()) + 1) % 2]
-            selected_item.setBackground(color)
-        g_world.selected_room = selected_item.text()
-        g_world.informations.start = str(g_world.selected_room)
-        g_world.notify()
+        selected_item.setBackground(Qt.yellow)
+        self.selected_room = g_project.data['rooms'][str(selected_item.text())]
+        g_project.data['world'].start = str(self.selected_room.id)
+        g_project.notify()
 
 
-    def update(self):
+    def update_data(self):
         list_lenght = self.rooms_list.count()
         while list_lenght:
             self.rooms_list.takeItem(0)
             list_lenght -= 1
-        for key, value in g_world.rooms.items():
+        for key, value in g_project.data['rooms'].items():
             room_item = QListWidgetItem(QIcon(QPixmap.fromImage(QImage(value.bg))),
                                         key)
             self.rooms_list.addItem(room_item)
-            if room_item.text() == g_world.informations.start:
+            if room_item.text() == self.selected_room.id:
                 room_item.setBackgroundColor(Qt.yellow)
-        item = self.rooms_list.findItems(g_world.selected_room,
+        self.rooms_list.findItems(self.selected_room.id,
                                   Qt.MatchFixedString)[0].setSelected(True)
