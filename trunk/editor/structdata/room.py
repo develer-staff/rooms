5868ce9776898f194919b886e1e9649b27123353
Matteo Baracani <bara@develer.com>
2012-03-14 11:31:01 +0100

    add new method addArea used to add a new area in areas list

--- trunk/editor/structdata/room.py
+++ trunk/editor/structdata/room.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from origin import OriginData
 
+from area import Area
+
 class Room(OriginData):
 
     tag_name = 'room'
@@ -10,3 +12,12 @@ class Room(OriginData):
         self.bg = bg
         self.areas = []
         self.bgm = bgm
+
+    def addArea(self, x, y, width, height, event=""):
+        number_of_new_area = 0
+        for area in self.areas:
+            if area.id.find("new_area") != -1:
+                number_of_new_area += 1
+        area = Area("new_area_%d" % (number_of_new_area + 1), str(x), str(y),
+                    str(width), str(height), event)
+        self.areas.append(area)
