652be8861d8a2c5a8831b8d69537efa572b0cb53
Matteo Baracani <bara@develer.com>
2012-03-16 14:12:56 +0100

    remove unused addNewRoom function

--- trunk/editor/structdata/project.py
+++ trunk/editor/structdata/project.py
@@ -4,7 +4,6 @@
 from misc.odict import OrderedDict
 
 from subject import Subject
-from room import Room
 
 class Project(Subject):
     def __init__(self):
@@ -16,15 +15,6 @@ class Project(Subject):
         self.data['vars'] = {}
         self.data['events'] = OrderedDict()
         self.data['rooms'] = OrderedDict()
-
-    def addNewRoom(self):
-        number_of_new_room = 0
-        for key in self.data['rooms'].keys():
-            if key.find("new_room") != -1:
-                number_of_new_room += 1
-        room = Room("new_room_%d" % (number_of_new_room + 1), "", "")
-        self.data['rooms'][room.id] = room
-        self.notify()
 
 g_project = Project()
 
