2a0e08396eca199bc3d42f3b77103b0fd1bf95cc
Matteo Baracani <bara@develer.com>
2012-03-16 09:32:05 +0100

    change variable name from dir to direction

--- trunk/editor/areaeditor.py
+++ trunk/editor/areaeditor.py
@@ -12,11 +12,12 @@ class AreaEditor(QWidget):
         self.area = area
         self.setMinimumSize(self.toAbsolute(self.area.width, 'x'),
                             self.toAbsolute(self.area.height, 'y'))
+        self.update()
 
-    def toAbsolute(self, value, dir):
-        if dir == 'x':
+    def toAbsolute(self, value, direction):
+        if direction == 'x':
             w = float(g_project.data['world'].width)
             return float(value) * w
-        elif dir == 'y':
+        elif direction == 'y':
             h = float(g_project.data['world'].height)
             return float(value) * h
