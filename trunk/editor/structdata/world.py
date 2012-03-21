9fadb07c70d5b910d384990102683999cf74ab91
Matteo Baracani <bara@develer.com>
2012-03-21 17:46:39 +0100

    add staticmethod create

--- trunk/editor/structdata/world.py
+++ trunk/editor/structdata/world.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python
 
 from origin import OriginData
+from project import g_project
 
 class World(OriginData):
 
     tag_name = 'world'
 
     def __init__(self, version, name, width, height, start):
-
         self.version = version
         self.name = name
         self.width = width
         self.height = height
         self.start = start
+
+    @staticmethod
+    def create(version, name, width, height, start):
+        return World(version, name, width, height, start)
