cc857044462fcd3d49aaa047e30b88789145f838
Matteo Baracani <bara@develer.com>
2012-03-23 15:48:42 +0100

    add search in list using search_line, when the table is created or cleared the signal are now blocked

--- trunk/editor/roomslistwidget.py
+++ trunk/editor/roomslistwidget.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 
+from contextlib import contextmanager
+
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
+
+@contextmanager
+def blockedSignals(widget):
+    widget.blockSignals(True)
+    try:
+        yield
+    finally:
+        widget.blockSignals(False)
 
 class RoomsListWidget(QWidget):
     """
@@ -19,24 +29,38 @@ class RoomsListWidget(QWidget):
         self.setMinimumSize(300, 300)
         self.event = event
         self.item = item
+        self.substring = ""
         self.search_line = QLineEdit(self)
         self.gl = QGridLayout()
         self.gl.addWidget(self.search_line, 0, 0)
         self.table = QTableWidget(self)
+        self.createSignals()
+        with blockedSignals(self.table):
+            self.createTable()
+        self.setLayout(self.gl)
+        self.connect(self.search_line, SIGNAL("textEdited(const QString &)"),
+                     self.changeTable)
+
+    def changeTable(self, substring):
+        self.substring = str(substring)
+        with blockedSignals(self.table):
+            self.table.clear()
+            self.table.setRowCount(0)
+            self.createTable()
+
+    def createTable(self):
         i = 0
         self.table.setSortingEnabled(False)
         self.table.setColumnCount(2)
         self.table.setHorizontalHeaderLabels(self.verticalHeader())
         for item in self.firstColumn():
-            self.table.setRowCount(self.table.rowCount() + 1)
-            self.table.setCellWidget(i, 0, QLabel(item))
-            self.table.setItem(i, 1, self.getSecondColumnItem(item))
-            self.table.setVerticalHeaderItem(i, QTableWidgetItem(""))
-            i += 1
+            if self.substring in item:
+                self.table.setRowCount(self.table.rowCount() + 1)
+                self.table.setCellWidget(i, 0, QLabel(item))
+                self.table.setItem(i, 1, self.getSecondColumnItem(item))
+                self.table.setVerticalHeaderItem(i, QTableWidgetItem(""))
+                i += 1
         self.gl.addWidget(self.table, 1, 0, 1, 2)
-        self.setLayout(self.gl)
-        self.createSignals()
-
 
     def createSignals(self):
         raise NotImplementedError
