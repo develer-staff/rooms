dfac7280ff3366e5885d0d5f5f61847e08220790
Matteo Baracani <bara@develer.com>
2012-03-24 11:50:16 +0100

    add class to set var requirment, it inherits from VarsListWidget

---
+++ trunk/editor/varreqlistwidget.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+
+from structdata import g_project
+from structdata import Param
+
+from varslistwidget import VarsListWidget
+
+class VarReqListWidget(VarsListWidget):
+    """
+    classe che deriva da VarsListWidget ed e' specifica per lavorare con
+    action di tipo VAR_REQ
+    """
+    def secondColumnItem(self, id_item):
+        for requirement in self.event.requirements:
+            if requirement.tag_name.upper() == "VAR_REQ":
+                if requirement.id == id_item:
+                    return requirement.value
+        return ""
