0d42482815494928c778b9f6b23fc46120341ba7
Matteo Baracani <bara@develer.com>
2012-03-26 15:17:34 +0200

    remove changeSelection function and change setRowSelected function: search the item request in table and select its row

--- trunk/editor/itemreqlistwidget.py
+++ trunk/editor/itemreqlistwidget.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
 from itemslistwidget import ItemsListWidget
 
 class ItemReqListWidget(ItemsListWidget):
@@ -8,21 +11,9 @@ class ItemReqListWidget(ItemsListWidget):
     classe utilizzata per la visualizzazione degli item_req
     """
 
-    def changeSelection(self, row, column):
-        selection = self.table.selectedRanges()
-        for sel in selection:
-            if (sel.bottomRow() == row and sel.columnCount() == 2):
-                index = selection.index(sel)
-                selection.pop(index)
-                self.table.setRangeSelected(selection)
-                break
-            elif sel.bottomRow() == row and sel.columnCount() == 1 and\
-                 sel.leftColumn() == column:
-                self.table.selectRow(row)
-                break
-
     def setRowSelected(self, id_item):
-        row = self.table.rowCount() - 1
-        for requirement in self.event.requirements:
-            if requirement.tag_name == "item_req" and requirement.id == id_item:
+        if self.item is not None:
+            item = self.table.findItems(id_item, Qt.MatchExactly)
+            if item:
+                row = item[0].row()
                 self.table.selectRow(row)
