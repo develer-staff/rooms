822495f8f0d3942a416236b56f92b4df55af1c65
Matteo Baracani <bara@develer.com>
2012-03-26 18:23:54 +0200

    change function for select initial element in room's list; now search in all event's requirements and find the correct room to select

--- trunk/editor/roomreqlistwidget.py
+++ trunk/editor/roomreqlistwidget.py
@@ -7,13 +7,16 @@ from roomlistwidget import RoomListWidget
 
 class RoomReqListWidget(RoomListWidget):
 
-
     def getIconSize(self):
         return QSize(100, 100)
 
-    def setRowSelected(self, id_item):
+    def setRowSelected(self, id_room):
         if self.item is not None:
-            item = self.table.findItems(id_item, Qt.MatchExactly)
-            if item:
-                row = item[0].row()
-                self.table.selectRow(row)
+            for requirement in self.event.requirements:
+                if requirement.tag_name == "var_req" and\
+                   requirement.value == id_room:
+                    item = self.table.findItems(id_room, Qt.MatchExactly)
+                    if item:
+                        row = item[0].row()
+                        self.table.selectRow(row)
+                        break
