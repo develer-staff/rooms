8dfb940cf99e7caf63c3454f057d6019a391892c
Matteo Baracani <bara@develer.com>
2012-03-27 09:34:19 +0200

    add docstring

--- trunk/editor/roomlistwidget.py
+++ trunk/editor/roomlistwidget.py
@@ -9,6 +9,14 @@ from structdata import g_project
 
 class RoomListWidget(RoomItemListWidget):
 
+    """
+    Classe generica per la visualizzazione delle room. Eredita dalla classe
+    RoomItemListWidget e implementa i metodi specifici necessari per la
+    visualizzazione delle room. Le classi derivate devono andare a implementare
+    la funzione getRowSelected che indica quale riga della tabella deve essere
+    selezionata appena la tabella viene mostrata
+    """
+
     def firstColumn(self):
         return g_project.data['rooms'].keys()
 
