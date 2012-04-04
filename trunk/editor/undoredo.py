7bfb504abb78d97f1b329c5bd60bd06ff257099a
Matteo Baracani <bara@develer.com>
2012-04-04 17:39:27 +0200

    add moreRedo method to know if there are more redo step and redo function to have the next pass. Implement even the moreredo method to know if there are more redo step

--- trunk/editor/undoredo.py
+++ trunk/editor/undoredo.py
@@ -12,19 +12,26 @@ class UndoRedo(object):
 
 
     def __init__(self):
+        g_project.subscribe(self)
         self._list_of_project = []
         self._add_element = True
-        self._list_index = 0
+        self._list_index = -1
         self._list_of_room = []
-        g_project.subscribe(self)
+        self._subscibers = set()
+
 
     def undo(self):
         self._list_index -= 1
         self._add_element = False
-        openRooms(self._list_of_project[self._list_index - 1])
+        openRooms(self._list_of_project[self._list_index ])
+
+    def redo(self):
+        self._add_element = False
+        openRooms(self._list_of_project[self._list_index])
+        self._list_index += 1
 
     def getCurrentRoom(self):
-        room_id = self._list_of_room[self._list_index - 1]
+        room_id = self._list_of_room[self._list_index]
         room = g_project.data['rooms'][room_id]
         return room
 
@@ -37,15 +44,22 @@ class UndoRedo(object):
     def moreUndo(self):
         if len(self._list_of_project) == 0:
             return False
-        return (self._list_index > 1)
+        return (self._list_index > 0)
 
+    def moreRedo(self):
+        return (self._list_index < len(self._list_of_project))
 
     def updateData(self):
+
         if self._add_element:
             self._list_index += 1
-            self._list_of_project = self._list_of_project[:self._list_index]
-            self._list_of_room = self._list_of_room[:self._list_index]
+            if len(self._list_of_project):
+                self._list_of_project = self._list_of_project[:self._list_index + 1]
+                self._list_of_room = self._list_of_room[:self._list_index + 1]
             self._list_of_project.append(saveRooms())
         self._add_element = True
+        self.notify()
+
+
 
 g_undoredo = UndoRedo()
