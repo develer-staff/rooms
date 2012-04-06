df4c726b6c9845b1f31050e8ffe25a467a33be35
Matteo Baracani <bara@develer.com>
2012-04-06 16:19:53 +0200

    update docstring

--- trunk/editor/utils.py
+++ trunk/editor/utils.py
@@ -15,10 +15,23 @@ def blockedSignals(widget):
 
 class PathTransform(object):
 
+    """
+    classe per trasformare il path di un file da relativo ad assoluto, questo
+    per recuperare file che si trovano in una directory diversa da quella
+    dell'editor. La classe mette a disposizione una variabile globale
+    g_ptransform per implemetare il template singleton.
+    Nel momento che si cambia il path del file .rooms su cui si lavora si
+    deve settare la variabile path_file
+    """
     def __init__(self):
         self.path_file = None
 
     def relativeToAbsolute(self, path_file):
+        """
+        funzione per la conversione del path da relativo ad assoluto.
+        Se non e' stato settato il path del file .rooms viene lanciata
+        un'eccezione di tipo AssertError
+        """
         assert self.path_file is not None
         path = join(self.path_file, path_file)
         return normpath(path)
