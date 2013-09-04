6028483784b2d2197d7063e177ab105d49c6a90f
Riccardo Ferrazzo <rferrazz@develer.com>
2013-09-04 16:48:48 +0200

    cppunit added to lib

---
+++ engine/unittest/tests/main.cpp
@@ -0,0 +1,15 @@
+#include "tests.h"
+
+#include <cppunit/ui/text/TestRunner.h>
+
+int main()
+{
+    CppUnit::TextUi::TestRunner runner;
+    runner.addTest(ActionTests::suite());
+    runner.addTest(RoomTests::suite());
+    runner.addTest(ApiTests::suite());
+    runner.addTest(WorldTests::suite());
+    runner.addTest(XmlTests::suite());
+    runner.run();
+    return 0;
+}
---
+++ engine/unittest/tests/tests.h
@@ -0,0 +1,9 @@
+#ifndef TESTS_H_INCLUDED
+#define TESTS_H_INCLUDED
+
+#include "actiontests.h"
+#include "roomtests.h"
+#include "enginetests.h"
+#include "xmltests.h"
+
+#endif // TESTS_H_INCLUDED
