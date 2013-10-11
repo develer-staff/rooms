a51ff8b63653539a4a34b4a2498d86c0b0d04306
Riccardo Ferrazzo <rferrazz@develer.com>
2013-10-11 14:14:09 +0200

    Android can play cutscenes

--- engine/android-frontend/main.cpp
+++ engine/android-frontend/main.cpp
@@ -5,10 +5,56 @@
 #include "qtquick2applicationviewer.h"
 
 #include <QtGui/QGuiApplication>
+#include <QXmlStreamReader>
 #include <QtQml>
 #include <QDebug>
 
 Engine engine;
+
+
+// This workaround is needed because Rooms engine cannot read
+// from Android assets pseudo-folder
+void preloadCutscene(const QString &worldContent){
+    QXmlStreamReader xml(worldContent);
+    QStringList csList;
+    while(!xml.atEnd()){
+        QXmlStreamReader::TokenType token = xml.readNext();
+
+        if (token == QXmlStreamReader::StartDocument)
+            continue;
+
+        if (token == QXmlStreamReader::StartElement){
+            if (xml.name() == "action" && xml.attributes().value("id") == "CUTSCENE_START"){
+                while(!(xml.tokenType() == QXmlStreamReader::EndElement && xml.name() == "action")){
+                    xml.readNext();
+                    if (xml.name() != "param")
+                        continue;
+                    QString csPath = xml.attributes().value("value").toString();
+                    QFile csFile(csPath);
+                    if (!csFile.exists()){
+                        if (QFile::copy(QString("assets:/%1").arg(csPath), csPath))
+                            csList.append(csPath);
+                    }
+                }
+            }
+        }
+    }
+
+    QRegExp rx("###[^\"]+\"([^\"]+)\"");
+    foreach (const QString &f, csList){
+        QFile file(f);
+        file.open(QIODevice::ReadOnly | QIODevice::Text);
+        QString txt = file.readAll();
+        file.close();
+        if(rx.indexIn(txt) != -1){
+            QFile csdFile(rx.cap(1));
+            if (!csdFile.exists()) {
+                qDebug() << rx.cap(1);
+                QFile::copy(QString("assets:/%1").arg(rx.cap(1)), rx.cap(1));
+            }
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -28,6 +74,8 @@ int main(int argc, char *argv[])
         qDebug() << "Error loading world";
         app.exit(1);
     }
+
+    preloadCutscene(descr_string);
 
     qmlRegisterType<Controller>("rooms", 1, 0, "Controller");
     qmlRegisterType<QGuiData>("rooms", 1, 0, "GuiData");
